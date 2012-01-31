90ad606d9ba4c9a41b27f5ad70f3e0384eebcc5e
Tobias Schneider <schneider@jancona.com>
2012-01-31 10:55:05 +0100

    add simple SWF.embed

---
+++ src/swf/swf.js
@@ -0,0 +1,21 @@
+/* -*- mode: javascript; tab-width: 4; insert-tabs-mode: nil; indent-tabs-mode: nil -*- */
+
+SWF = {
+  embed: function(url, stage) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function() {
+      var result = SWF.parse(xhr.response);
+      var header = result.header;
+      var bounds = header.bounds;
+
+      var canvas = document.createElement('canvas');
+      canvas.width = (bounds.xMax - bounds.xMin) / 20;
+      canvas.height = (bounds.yMax - bounds.yMin) / 20;
+
+      stage.appendChild(canvas);
+    };
+    xhr.send();
+  }
+};
