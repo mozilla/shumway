40b74ac06e6aa93074ff8e4f628c6ed15e93e51d
Tobias Schneider <schneider@jancona.com>
2012-01-31 10:49:05 +0100

    add Stream class

---
+++ src/swf/parser/stream.js
@@ -0,0 +1,23 @@
+/* -*- mode: javascript; tab-width: 4; insert-tabs-mode: nil; indent-tabs-mode: nil -*- */
+
+function Stream(buffer, byteOffset, byteLength) {
+  if (!byteOffset)
+    byteOffset = 0;
+  if (!byteLength)
+    byteLength = buffer.byteLength - byteOffset;
+  var stream = new DataView(buffer, byteOffset, byteLength);
+  stream.__proto__ = Stream.prototype;
+  stream.bytes = new Uint8Array(buffer, byteOffset, byteLength);
+  stream.pos = 0;
+  stream.bitBuffer = 0;
+  stream.bitLength = 0;
+  return stream;
+}
+var proto = Stream.prototype = Object.create(DataView.prototype);
+
+proto.align = function() {
+  this.bitBuffer = this.bitLength = 0;
+}
+proto.substream = function(begin, end) {
+  return new Stream(this.buffer, this.byteOffset + begin, end - begin);
+}
