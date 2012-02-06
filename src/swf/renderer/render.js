11b1bf956eadf18d9ca69167940c3d16e5461804
Tobias Schneider <schneider@jancona.com>
2012-02-06 20:13:11 +0100

    do simple rendering

--- src/swf/renderer/render.js
+++ src/swf/renderer/render.js
@@ -1,22 +1,11 @@
 /* -*- mode: javascript; tab-width: 4; insert-tabs-mode: nil; indent-tabs-mode: nil -*- */
 
-var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                        		window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
-SWF.render = function(stage, displayList, rate) {
-	var ctx = stage.getContext('2d');
-	(function loop() {
-		if (displayList.dirty) {
-			delete displayList.dirty;
-			var depths = keys(displayList);
-			depths.sort();
-			console.time('frame');
-			var i = 0;
-			var depth;
-			while (depth = depths[i++])
-				displayList[depth].render(ctx);
-			console.timeEnd('frame');
-		}
-		requestAnimationFrame(loop);
-	})();
+function renderDisplayList(displayList, ctx) {
+  var maxDepth = displayList.maxDepth;
+	var depth = 0;
+	while (depth++ < maxDepth) {
+    var character = displayList[depth];
+    if (character)
+		  character.render(ctx);
+  }
 };
