71f0ceb36662485ec364b2cbebd4fde3c370c622
Tobias Schneider <schneider@jancona.com>
2012-02-22 00:24:20 +0100

    build dictionary and root timeline

--- src/swf/swf.js
+++ src/swf/swf.js
@@ -2,6 +2,37 @@
 
 SWF = {
   embed: function(file, stage) {
-    worker.postMessage(file);
+    var dictionary = { };
+    var timeline = [];
+    var displayList = { };
+    work(file, function(obj) {
+      if (obj) {
+        if (obj.id) {
+          dictionary[obj.id] = obj;
+        } else if (obj.type === 'pframe') {
+          for (var depth in obj) {
+            if (+depth) {
+              var entry = obj[depth];
+              depth -= 0x4001;
+              if (entry) {
+                if (entry.move)
+                  var character = create(displayList[depth])
+                else
+                  var character = create(dictionary[entry.id])
+                for (var prop in entry)
+                  character[prop] = entry[prop];
+                displayList[depth] = character;
+              } else {
+                displayList[depth] = entry;
+              }
+            }
+          }
+          var i = obj.repeat || 1;
+          while (i--)
+            timeline.push(displayList);
+          displayList = create(displayList);
+        }
+      }
+    });
   }
 };
