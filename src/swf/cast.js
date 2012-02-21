7aa579deb61478ec64348c3ca14fc8b6b2be7768
Tobias Schneider <schneider@jancona.com>
2012-02-21 17:58:11 +0100

    optimize object casting

--- src/swf/cast.js
+++ src/swf/cast.js
@@ -1,68 +1,75 @@
 /* -*- mode: javascript; tab-width: 4; insert-tabs-mode: nil; indent-tabs-mode: nil -*- */
 
-SWF.cast = function cast(tags, dictionary, serialized) {
-  if (isArray(tags)) {
-    var frame = { type: 'pframe' };
-    var i = 0;
-    var tag;
-    while (tag = tags[i++]) {
+function cast(tags, dictionary) {
+  if (!isArray(tags))
+    tags = [tags];
+  var pframe = { type: 'pframe' };
+  for (var i = 0, tag; tag = tags[i]; ++i) {
+    if (tag.type === 'frame')
+      break;
+    if (tag.id) {
       switch (tag.type) {
-      case 'frame':
-        return frame;
-      case 'place':
-        var depth = tag.depth;
-        frame[depth] = tag;
+      case 'font':
+        var obj = defineFont(tag, dictionary);
         break;
-      case 'remove':
-        frame[tag.depth] = null;
+      case 'shape':
+        var obj = defineShape(tag, dictionary);
         break;
-      }
-    }
-  }
-  var tag = tags;
-  var id = tag.id;
-  switch (tag.type) {
-  case 'font':
-    var obj = defineFont(tag, dictionary);
-    break;
-  case 'shape':
-    var obj = defineShape(tag, dictionary);
-    break;
-  case 'sprite':
-    var frames = [];
-    var tags = tag.tags;
-    var i = 0;
-    var n = 0;
-    var controlTags = [];
-    while (tag = tags[i++]) {
-      if (tag.type === 'frame') {
-        frames[n] = cast(controlTags, dictionary);
-        ++n;
-        controlTags = [];
-        while (tag = tags[i]) {
-          if (tag.type === 'frame') {
-            ++n;
-            ++i;
+      case 'sprite':
+        var pframes = [];
+        var subtags = tag.tags;
+        var j = 0;
+        var subtag;
+        var controlTags = [];
+        while (subtag = subtags[j++]) {
+          if (subtag.type === 'frame') {
+            var k = j;
+            do {
+              controlTags.push(subtag);
+              subtag = subtags[k++];
+            } while (subtag && subtag.type === 'frame');
+            j = k;
+            pframes.push(cast(controlTags, dictionary));
+            controlTags = [];
           } else {
-            break;
+            controlTags.push(subtag);
           }
         }
-      } else {
-        controlTags.push(tag);
+        var obj = {
+          type: 'clip',
+          id: tag.id,
+          pframes: pframes
+        };
+        break;
+      case 'text':
+        var obj = defineText(tag, dictionary);
+        break;
+      default:
+        var obj = tag;
       }
+      dictionary[tag.id] = obj;
+      continue;
     }
-    var obj = {
-      type: 'movieclip',
-      id: id,
-      frames: frames
-    };
-    break;
-  case 'text':
-    var obj = defineText(tag, dictionary);
-    break;
-  default:
-    var obj = tag;
+    switch (tag.type) {
+    case 'place':
+      var entry = { move: tag.move };
+      if (tag.place)
+        entry.id = tag.objId;
+      if (tag.hasMatrix)
+        entry.matrix = tag.matrix;
+      if (tag.hasRatio)
+        entry.ratio = tag.ratio / 0xffff;
+      pframe[tag.depth] = entry;
+      break;
+    case 'remove':
+      pframe[tag.depth] = null;
+      break;
+    }
   }
-  dictionary[id] = serialized ? obj : SWF.deserialize(obj);
-  return dictionary[id];
+  if (i === 1 && obj)
+    return obj;
+  for (var n = 0; (tag = tags[i]) && tag.type === 'frame'; ++n, ++i);
+  if (n > 1)
+    pframe.repeat = n;
+  return pframe;
 };
