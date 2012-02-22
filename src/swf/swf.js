bb7a1486a1f3eb8a1b8d1800491efd53f9b70801
Tobias Schneider <schneider@jancona.com>
2012-02-22 12:17:02 +0100

    move embed code to separate file

--- src/swf/swf.js
+++ src/swf/swf.js
@@ -1,38 +1,3 @@
 /* -*- mode: javascript; tab-width: 4; insert-tabs-mode: nil; indent-tabs-mode: nil -*- */
 
-SWF = {
-  embed: function(file, stage) {
-    var dictionary = { };
-    var timeline = [];
-    var displayList = { };
-    work(file, function(obj) {
-      if (obj) {
-        if (obj.id) {
-          dictionary[obj.id] = obj;
-        } else if (obj.type === 'pframe') {
-          for (var depth in obj) {
-            if (+depth) {
-              var entry = obj[depth];
-              depth -= 0x4001;
-              if (entry) {
-                if (entry.move)
-                  var character = create(displayList[depth])
-                else
-                  var character = create(dictionary[entry.id])
-                for (var prop in entry)
-                  character[prop] = entry[prop];
-                displayList[depth] = character;
-              } else {
-                displayList[depth] = entry;
-              }
-            }
-          }
-          var i = obj.repeat || 1;
-          while (i--)
-            timeline.push(displayList);
-          displayList = create(displayList);
-        }
-      }
-    });
-  }
-};
+self.SWF = { };
