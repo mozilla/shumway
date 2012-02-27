18c2992f9636b13b14d2357b619fd6df0898c348
Tobias Schneider <schneider@jancona.com>
2012-02-27 18:28:44 +0100

    improve renderer

--- src/swf/renderer.js
+++ src/swf/renderer.js
@@ -1,51 +1,50 @@
 /* -*- mode: javascript; tab-width: 4; insert-tabs-mode: nil; indent-tabs-mode: nil -*- */
 
-var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame;  
+var requestAnimationFrame = window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame;
 
-SWF.display = function(displayList, ctx) {
-  displayList.ctx = ctx;
-  function draw() {
-    if (displayList.ctx) {
-      if (displayList.dirty) {
-        var depths = keys(displayList);
-        var i = 0;
-        var depth;
-        while (depth = depths[i++]) {
-          var character = displayList[depth];
-          if (character && character.render)
-            character.render(ctx, character.matrix, character.ratio);
-        }
-      }
-      displayList.dirty = false;
-      requestAnimationFrame(draw);
-    }
-  }
-  requestAnimationFrame(draw);
-};
-SWF.free = function(displayList) {
-  delete displayList.ctx;
-};
-SWF.render = function(displayList, dictionary, frame) {
-  var depths = keys(frame);
+function render(displayList, ctx) {
+  ctx.clearRect(0, 0, ctx.canvas.width * 20, ctx.canvas.height * 20);
+  var depths = [];
+  var i = 0;
+  for (depths[i++] in displayList);
+  depths.sort(function(a, b) {
+    return a - b;
+  });
   var i = 0;
   var depth;
   while (depth = depths[i++]) {
-    var tag = frame[depth];
-    if (tag) {
-      if (tag.move) {
-        var character = create(displayList[depth]);
-        for (var prop in tag)
-          character[prop] = tag[prop];
-        } else {
-          var character = create(tag);
-        }
-        var obj = dictionary[character.objId];
-        character.render = obj.render;
-        displayList[depth] = character;
-    } else {
-      delete displayList[depth];
+    var character = displayList[depth];
+    if (character) {
+      ctx.save();
+      var matrix = character.transform.matrix;
+      if (matrix) {
+        ctx.transform(
+          matrix.scaleX,
+          matrix.skew0,
+          matrix.skew1,
+          matrix.scaleY,
+          matrix.translateX,
+          matrix.translateY
+        );
+      }
+      if (character.draw)
+        character.draw(ctx, character.ratio);
+      else if (character.nextFrame)
+        character.nextFrame.call(render, ctx);
+      ctx.restore();
     }
   }
-  if (depths.length)
-    displayList.dirty = true;
-};
+}
+function renderMovieClip(mc, rate, ctx) {
+  ctx.scale(0.05, 0.05);
+  var maxDelay = 1000 / rate;
+  var frameTime = 0;
+  (function draw() {
+    var now = +new Date;
+    if (now - frameTime >= maxDelay) {
+      frameTime = now;
+      mc.nextFrame.call(render, ctx);
+    }
+    requestAnimationFrame(draw);
+  })();
+}
