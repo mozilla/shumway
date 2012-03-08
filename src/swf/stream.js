5d8d33a6144e4068ff7476fe2edb7afcaf0a79ee
Tobias Schneider <schneider@jancona.com>
2012-03-08 18:00:39 +0100

    optimize Stream and prepare support for multiple compression algorithms

--- src/swf/stream.js
+++ src/swf/stream.js
@@ -1,25 +1,36 @@
 /* -*- mode: javascript; tab-width: 4; insert-tabs-mode: nil; indent-tabs-mode: nil -*- */
 
-function Stream(buffer, offset, length) {
+function Stream(buffer, offset, length, compression) {
   if (offset === undefined)
     offset = 0;
+  if (buffer.buffer instanceof ArrayBuffer) {
+	offset += buffer.byteOffset;
+    buffer = buffer.buffer;
+  }
   if (length === undefined)
     length = buffer.byteLength - offset;
-  if (offset + length > buffer.byteLength) {
+
+  if (compression === 'C') {
     var bytes = new Uint8Array(length);
     var stream = new DataView(bytes.buffer);
     stream.realLength = 0;
-    var sstream = stream.sstream = new Stream(buffer, offset);
+    var sstream = new Stream(buffer, offset);
     var header = sstream.getUint16(sstream.pos);
     assert((header & 0x0f00) === 0x0800, 'unknown compression method', 'inflate');
     assert(!(header % 31), 'bad FCHECK', 'inflate');
     assert(!(header & 0x20), 'FDICT bit set', 'inflate');
     sstream.pos += 2;
+    stream.ensure = function(length) {
+      var index = this.pos + length;
+      while (this.realLength < index)
+        inflateBlock(sstream.bytes, sstream, this.bytes, this);
+    };
   } else {
     var bytes = new Uint8Array(buffer, offset, length);
     var stream = new DataView(buffer, offset, length);
     stream.realLength = length;
   }
+
   stream.__proto__ = Stream.prototype;
   stream.bytes = bytes;
   stream.pos = 0;
@@ -28,24 +39,17 @@ function Stream(buffer, offset, length) {
   stream.bitLength = 0;
   return stream;
 }
-var proto = Stream.prototype = Object.create(DataView.prototype);
+var proto = Stream.prototype = create(DataView.prototype);
 
 proto.align = function() {
   this.bitBuffer = this.bitLength = 0;
 };
-proto.ensure = function(length) {
-  var sstream = this.sstream;
-  if (sstream) {
-    var index = this.pos + length;
-    while (this.realLength < index)
-      inflateBlock(sstream.bytes, sstream, this.bytes, this);
-  }
-};
+proto.ensure = function() { };
 proto.remaining = function() {
   return this.end - this.pos;
 };
 proto.substream = function(begin, end) {
-  var stream = Object.create(this);
+  var stream = create(this);
   stream.pos = begin;
   stream.end = end;
   stream.align();
