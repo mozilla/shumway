54363c1675494b196f69a741c68200d95e6727af
Yury Delendik <ydelendik@mozilla.com>
2012-06-06 19:15:39 -0500

    Simulates WeakMap for Chrome

--- src/swf/util.js
+++ src/swf/util.js
@@ -121,3 +121,29 @@ Promise.all = function(promises, collectResults) {
   }
   return promise;
 };
+
+(function checkWeakMap() {
+  if (typeof this.WeakMap === 'function')
+    return; // weak map is supported
+
+  var id = 0;
+  function WeakMap() {
+    this.id = '$weekmap' + (id++);
+  };
+  WeakMap.prototype = {
+    has: function(obj) {
+      return this.id in obj;
+    },
+    get: function(obj, defaultValue) {
+      return this.id in obj ? obj[this.id] : defaultValue;
+    },
+    set: function(obj, value) {
+      Object.defineProperty(obj, this.id, {
+        value: value,
+        enumerable: false,
+        configurable: true
+      });
+    }
+  };
+  this.WeakMap = WeakMap;
+})();
