2a5358763485ca4424050e666e7b22f9e47a1fb2
Liviu Codrut Stancu <lstancu@host-2-178.mv.mozilla.com>
2012-07-09 15:47:02 -0700

    Bug fix in graph viz output.

--- src/avm2/bin/avm.js
+++ src/avm2/bin/avm.js
@@ -144,7 +144,7 @@ function processAbc(abc) {
       method.analysis.analyzeControlFlow();
       method.analysis.restructureControlFlow();
       if (method.analysis) {
-        method.analysis.traceCFG(writer, method, "G" + graph + "_");
+        method.analysis.traceCFG(stdout, method, "G" + graph + "_");
         graph += 1;
       }
     });
