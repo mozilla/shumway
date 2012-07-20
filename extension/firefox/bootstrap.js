ab5ac5700c4d88e9c94e7e9992bcd398a5b3ff84
Yury Delendik <ydelendik@mozilla.com>
2012-07-20 13:06:37 -0500

    Add registerOverlayPreview handling

--- extension/firefox/bootstrap.js
+++ extension/firefox/bootstrap.js
@@ -4,52 +4,63 @@
 'use strict';
 
 const RESOURCE_NAME = 'shumway';
-const EXT_PREFIX = 'shumway@labs.mozilla.org';
+const EXT_PREFIX = 'shumway@research.mozilla.org';
 
 let Cc = Components.classes;
 let Ci = Components.interfaces;
 let Cm = Components.manager;
 let Cu = Components.utils;
 
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 Cu.import('resource://gre/modules/Services.jsm');
+
+let Ph = Cc["@mozilla.org/plugin/host;1"].getService(Ci.nsIPluginHost);
+let registerOverlayPreview = 'registerPlayPreviewMimeType' in Ph;
 
 function log(str) {
   dump(str + '\n');
 }
 
-// Register/unregister a class as a component.
-let Factory = {
-  registrar: null,
-  aClass: null,
-  register: function(aClass) {
-    if (this.aClass) {
-      log('Cannot register more than one class');
-      return;
-    }
-    this.registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
-    this.aClass = aClass;
-    var proto = aClass.prototype;
-    this.registrar.registerFactory(proto.classID, proto.classDescription,
-      proto.contractID, this);
+// Register/unregister a constructor as a component.
+function Factory() {}
+
+Factory.prototype = {
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIFactory]),
+  _targetConstructor: null,
+
+  register: function register(targetConstructor) {
+    this._targetConstructor = targetConstructor;
+    var proto = targetConstructor.prototype;
+    var registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.registerFactory(proto.classID, proto.classDescription,
+                              proto.contractID, this);
   },
-  unregister: function() {
-    if (!this.aClass) {
-      log('Class was never registered.');
-      return;
-    }
-    var proto = this.aClass.prototype;
-    this.registrar.unregisterFactory(proto.classID, this);
-    this.aClass = null;
+
+  unregister: function unregister() {
+    var proto = this._targetConstructor.prototype;
+    var registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.unregisterFactory(proto.classID, this);
+    this._targetConstructor = null;
   },
-  // nsIFactory::createInstance
-  createInstance: function(outer, iid) {
-    if (outer !== null)
+
+  // nsIFactory
+  createInstance: function createInstance(aOuter, iid) {
+    if (aOuter !== null)
       throw Cr.NS_ERROR_NO_AGGREGATION;
-    return (new (this.aClass)).QueryInterface(iid);
+    return (new (this._targetConstructor)).QueryInterface(iid);
+  },
+
+  // nsIFactory
+  lockFactory: function lockFactory(lock) {
+    // No longer used as of gecko 1.7.
+    throw Cr.NS_ERROR_NOT_IMPLEMENTED;
   }
 };
 
-let flashStreamConverterUrl = null;
+let factory1 = new Factory();
+let factory2 = new Factory();
+
+let FlashStreamConverterUrl = null;
 
 // As of Firefox 13 bootstrapped add-ons don't support automatic registering and
 // unregistering of resource urls and components/contracts. Until then we do
@@ -60,44 +71,43 @@ function startup(aData, aReason) {
   var ioService = Services.io;
   var resProt = ioService.getProtocolHandler('resource')
                   .QueryInterface(Ci.nsIResProtocolHandler);
-  var aliasFile = Cc['@mozilla.org/file/local;1']
-                    .createInstance(Ci.nsILocalFile);
-  var componentPath = aData.installPath.clone();
-  componentPath.append('content');
-  aliasFile.initWithPath(componentPath.path);
-  var aliasURI = ioService.newFileURI(aliasFile);
+  var aliasURI = ioService.newURI('content/', 'UTF-8', aData.resourceURI);
   resProt.setSubstitution(RESOURCE_NAME, aliasURI);
 
   // Load the component and register it.
-  flashStreamConverterUrl = aData.resourceURI.spec +
-                          'components/FlashStreamConverter.js';
-  Cu.import(flashStreamConverterUrl);
-  Factory.register(FlashStreamConverter);
-  Services.prefs.setBoolPref('extensions.shumway.active', true);
+  FlashStreamConverterUrl = aData.resourceURI.spec +
+    'components/FlashStreamConverter.js';
+  Cu.import(FlashStreamConverterUrl);
+  factory1.register(FlashStreamConverter1);
+  factory2.register(FlashStreamConverter2);
+
+  if (registerOverlayPreview)
+    Ph.registerPlayPreviewMimeType('application/x-shockwave-flash');
 }
 
 function shutdown(aData, aReason) {
-  if (Services.prefs.getBoolPref('extensions.shumway.active'))
-    Services.prefs.setBoolPref('extensions.shumway.active', false);
+  if (aReason == APP_SHUTDOWN)
+    return;
   var ioService = Services.io;
   var resProt = ioService.getProtocolHandler('resource')
                   .QueryInterface(Ci.nsIResProtocolHandler);
   // Remove the resource url.
   resProt.setSubstitution(RESOURCE_NAME, null);
   // Remove the contract/component.
-  Factory.unregister();
+  factory1.unregister();
+  factory2.unregister();
+
+  if (registerOverlayPreview)
+    Ph.unregisterPlayPreviewMimeType('application/x-shockwave-flash');
+
   // Unload the converter
-  if (flashStreamConverterUrl) {
-    Cu.unload(flashStreamConverterUrl);
-    flashStreamConverterUrl = null;
-  }
+  Cu.unload(FlashStreamConverterUrl);
+  FlashStreamConverterUrl = null;
 }
 
 function install(aData, aReason) {
-  Services.prefs.setBoolPref('extensions.shumway.active', false);
 }
 
 function uninstall(aData, aReason) {
-  Services.prefs.clearUserPref('extensions.shumway.active');
 }
 
