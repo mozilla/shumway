3a62c0eefa6e45017be9eab864d76a9b72beedb8
Tobias Schneider <schneider@jancona.com>
2012-08-06 23:01:09 +0200

    Refactor some utils.

--- src/flash/util.js
+++ src/flash/util.js
@@ -24,3 +24,45 @@ function describeProperty(val) {
 function illegalOperation() {
   throw Error('Illegal Operation');
 }
+
+function Promise() {
+  this.resolved = false;
+  this._callbacks = [];
+}
+Promise.when = function () {
+  var numPromises = arguments.length;
+  var promise = new Promise;
+  if (!numPromises) {
+    promise.resolve();
+  } else {
+    var values = [];
+    for (var i = 0; i < numPromises; i++) {
+      var arg = arguments[i];
+      arg.then(function (val) {
+        values.push(val);
+        if (!--numPromises)
+          promise.resolve.apply(promise, values);
+      });
+    }
+  }
+  return promise;
+};
+Promise.prototype.resolve = function (val) {
+  if (this.resolved)
+    return;
+
+  this.resolved = true;
+  this.value = val;
+
+  var callbacks = this._callbacks;
+  for (var i = 0, n = callbacks.length; i < n; i++) {
+    var cb = callbacks[i];
+    cb(val);
+  }
+};
+Promise.prototype.then = function (cb) {
+  if (this.resolved)
+    cb(this.value);
+  else
+   this._callbacks.push(cb);
+};
