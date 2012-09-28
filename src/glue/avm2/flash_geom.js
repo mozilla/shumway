1b99797d3293cfe9c14aa3f166aaf30d245f9b6f
Jet Villegas <jet@junglecode.net>
2012-09-27 17:04:10 -0700

    Add glue code for flash.geom classes.

--- src/glue/avm2/flash_geom.js
+++ src/glue/avm2/flash_geom.js
@@ -1,3 +1,29 @@
+natives.ColorTransformClass = function ColorTransformClass(runtime, scope, instance, baseClass) {
+  var c = new runtime.domain.system.Class("ColorTransform", instance, Domain.passthroughCallable(instance));
+  c.extend(baseClass, ColorTransform.prototype);
+
+  c.nativeStatics = {
+  };
+
+  c.nativeMethods = {
+  };
+
+  return c;
+};
+
+natives.MatrixClass = function MatrixClass(runtime, scope, instance, baseClass) {
+  var c = new runtime.domain.system.Class("Matrix", instance, Domain.passthroughCallable(instance));
+  c.extend(baseClass, Matrix.prototype);
+
+  c.nativeStatics = {
+  };
+
+  c.nativeMethods = {
+  };
+
+  return c;
+};
+
 natives.PointClass = function PointClass(runtime, scope, instance, baseClass) {
   var c = new runtime.domain.system.Class("Point", instance, Domain.passthroughCallable(instance));
   c.extend(baseClass, Point.prototype);
@@ -10,3 +36,30 @@ natives.PointClass = function PointClass(runtime, scope, instance, baseClass) {
 
   return c;
 };
+
+natives.RectangleClass = function RectangleClass(runtime, scope, instance, baseClass) {
+  var c = new runtime.domain.system.Class("Rectangle", instance, Domain.passthroughCallable(instance));
+  c.extend(baseClass, Rectangle.prototype);
+
+  c.nativeStatics = {
+  };
+
+  c.nativeMethods = {
+  };
+
+  return c;
+};
+
+natives.TransformClass = function TransformClass(runtime, scope, instance, baseClass) {
+  var c = new runtime.domain.system.Class("Transform", instance, Domain.passthroughCallable(instance));
+  c.extend(baseClass, Transform.prototype);
+
+  c.nativeStatics = {
+  };
+
+  c.nativeMethods = {
+  };
+
+  return c;
+};
+
