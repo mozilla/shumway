9f883cf3896775e1e56800d0aa3e1ba2df33bd7e
Tobias Schneider <schneider@jancona.com>
2012-10-10 18:50:49 +0200

    Remove debugger statement.

--- src/swf/embed.js
+++ src/swf/embed.js
@@ -46,7 +46,6 @@ SWF.embed = function(file, container, options) {
 
     // Dirty hack for now.
     //stage.addChild(loader.content);
-    debugger;
     stage._children[0] = loader._content;
     container.appendChild(canvas);
     renderStage(stage, ctx);
