3d6434f41b0d8877689d7d35f763eb9b440b17a7
Michael Bebenita <mbebenita@mozilla.com>
2012-10-10 04:01:41 -0700

    runtime: Define an Activation constructor.

--- src/avm2/runtime.js
+++ src/avm2/runtime.js
@@ -262,6 +262,10 @@ function checkFilter(value) {
   notImplemented("checkFilter");
 }
 
+function Activation (methodInfo) {
+  this.methodInfo = methodInfo;
+}
+
 var Interface = (function () {
   function Interface(classInfo) {
     var ii = classInfo.instanceInfo;
@@ -332,6 +336,19 @@ var Scope = (function () {
     this.global = parent ? parent.global : this;
     this.isWith = isWith;
   }
+
+  scope.prototype.findDepth = function findDepth(obj) {
+    var current = this;
+    var depth = 0;
+    while (current) {
+      if (current.object === obj) {
+        return depth;
+      }
+      depth ++;
+      current = current.parent;
+    }
+    return -1;
+  };
 
   scope.prototype.findProperty = function findProperty(mn, domain, strict) {
     assert(this.object);
@@ -744,7 +761,7 @@ var Runtime = (function () {
       mi.analysis = new Analysis(mi, { massage: true });
 
       if (mi.traits) {
-        mi.activationPrototype = this.applyTraits({}, null, null, mi.traits, null, false);
+        mi.activationPrototype = this.applyTraits(new Activation(mi), null, null, mi.traits, null, false);
       }
 
       // If we have exceptions, make the catch scopes now.
