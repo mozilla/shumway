87258511e75e418b5e9c2b4f3cd0b9abb7fecefa
Yury Delendik <ydelendik@mozilla.com>
2012-10-23 10:46:58 -0500

    Fixes undef public$text

--- src/flash/text/StaticText.js
+++ src/flash/text/StaticText.js
@@ -1,12 +1,22 @@
 const StaticTextDefinition = (function () {
   var def = {
-    __class__: 'flash.text.StaticText'
+    __class__: 'flash.text.StaticText',
+
+    get text() {
+      return this._text;
+    },
+    set text(val) {
+      this._text = val;
+    }
   };
 
+  const desc = Object.getOwnPropertyDescriptor;
+
   def.__glue__ = {
-    text: {
-      get: function () { return this.text; },
-      set: function (v) { this.text = v; }
+    native: {
+      instance: {
+        text: desc(def, "text")
+      }
     }
   };
 
--- src/flash/text/TextField.js
+++ src/flash/text/TextField.js
@@ -1,12 +1,22 @@
 const TextFieldDefinition = (function () {
   var def = {
-    __class__: 'flash.text.TextField'
+    __class__: 'flash.text.TextField',
+
+    get text() {
+      return this._text;
+    },
+    set text(val) {
+      this._text = val;
+    }
   };
 
+  const desc = Object.getOwnPropertyDescriptor;
+
   def.__glue__ = {
-    text: {
-      get: function () { return this.text; },
-      set: function (v) { this.text = v; }
+    native: {
+      instance: {
+        text: desc(def, "text")
+      }
     }
   };
 
