c34a0eba9063592449b81f66834292442f3acd71
Yury Delendik <ydelendik@mozilla.com>
2012-11-14 11:27:39 -0600

    Removes fallback support for __proto__

--- src/swf/util.js
+++ src/swf/util.js
@@ -89,36 +89,6 @@ function adler32(data) {
       var name = m && m[1] !== 'anonymous' ? m[1] : null;
       return (this.__name = name);
     },
-    set: function (value) {
-      this.__name = value;
-    },
-    configurable: true,
-    enumerable: false
-  });
-})();
-
-(function protoPropertySupport() {
-  var obj1 = { t: true }, obj2 = {};
-  obj2.__proto__ = obj1;
-  if (obj2.t) {
-    return; // __proto__ property is supported
-  }
-
-  Object.defineProperty(Object.prototype, '__proto__', {
-    get: function () {
-      return this.__proto;
-    },
-    set: function (value) {
-      this.__proto = value;
-      for (var i in value) {
-        if (i === '__name' || i === '__proto') continue;
-        var p = value, d;
-        while (!(d = Object.getOwnPropertyDescriptor(p, i))) {
-          p = Object.getPrototypeOf(p);
-        }
-        Object.defineProperty(this, i, d);
-      }
-    },
     configurable: true,
     enumerable: false
   });
