9d32211dcec054ac0142195ce6bd289b4a110643
Jasper St. Pierre <jstpierre@mecheye.net>
2012-11-16 21:27:08 -0500

    util: Remove dead, unused utility functions

--- src/flash/util.js
+++ src/flash/util.js
@@ -7,18 +7,8 @@ function describePrototype() {
 function describeAccessor(get, set) {
   return { get: get, set: set, configurable: true, enumerable: true };
 }
-function describeConst(val) {
-  return { value: val, configurable: true, enumerable: true };
-}
 function describeInternalProperty(val) {
   return { value: val, writable: true };
-}
-function describeLazyProperty(name, getter) {
-  return describeAccessor(function () {
-    var val = getter.call(this);
-    Object.defineProperty(this, name, describeProperty(val));
-    return val;
-  });
 }
 function describeMethod(fn) {
   return describeProperty(fn);
