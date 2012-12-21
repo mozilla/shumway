b36d165961abab5c98f2b71b774260b8283542c0
Michael Bebenita <mbebenita@mozilla.com>
2012-12-21 15:26:59 -0800

    Added missing file

---
+++ src/flash/media/ID3Info.js
@@ -0,0 +1,17 @@
+const ID3InfoDefinition = (function () {
+  return {
+    __glue__: {
+      script: {
+        instance: {
+          songName: "public songName",
+          genre: "public genre",
+          artist: "public artist",
+          track: "public track",
+          album: "public album",
+          year: "public year",
+          comment: "public comment"
+        }
+      }
+    }
+  };
+}).call(this);
