f5bd8061f2d75589b66e15dd45dcde75104cae0c
Michael Bebenita <mbebenita@mozilla.com>
2013-02-01 01:08:24 -0800

    Mock-up ColorMatrixFilter class with applyFilter/computeFilterBounds APIs.

--- src/flash/filters/ColorMatrixFilter.js
+++ src/flash/filters/ColorMatrixFilter.js
@@ -1,11 +1,32 @@
 var ColorMatrixFilterDefinition = (function () {
-  var def = {
-    __class__: 'flash.filters.ColorMatrix',
-
-    initialize: function () { }
+  return {
+    __class__: "flash.filters.ColorMatrixFilter",
+    initialize: function () {
+      this._matrix = null;
+    },
+    applyFilter: function (buffer, width, height) {
+      if (this._matrix) {
+        assert (buffer instanceof Uint8ClampedArray);
+        assert (buffer.length === width * height * 4);
+        colorFilter(buffer, width, height, this._matrix);
+      }
+    },
+    computeFilterBounds: function (bounds) {
+      return bounds;
+    },
+    __glue__: {
+      native: {
+        instance: {
+          matrix: {
+            get: function matrix() {
+              return this._matrix;
+            },
+            set: function matrix(value) {
+              this._matrix = value;
+            }
+          }
+        }
+      }
+    }
   };
-
-  def.__glue__ = { };
-
-  return def;
 }).call(this);
