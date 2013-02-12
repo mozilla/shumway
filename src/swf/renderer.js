mozilla/shumway

commit e1409cdd046dfc14da2683b7b51967c409007544

Clear current default path when drawing sink commands.

src/swf/renderer.js
@@ -25,7 +25,6 @@ function renderDisplayObject(child, ctx, transform, cxform, clip) {
           ctx.fillStyle = path.fillStyle;
           if (path.fillTransform) {
             var m = path.fillTransform;
-            ctx.beginPath();
             ctx.__draw__(path);
             ctx.save();
             ctx.transform(m.a, m.b, m.c, m.d, m.tx, m.ty);

lib/Kanvas/kanvas.js
@@ -431,8 +431,6 @@
 
     defineProperty(kanvas2dContextProto, '__draw__', {
       value: function (sink) {
-        this.beginPath();
-
         var cmds = sink.__cmds__;
         var data = sink.__data__;
         for (var i = 0, j = 0; i < cmds.length; i++) {
@@ -460,26 +458,34 @@
     });
 
     kanvas2dContextProto.fill = function (path) {
-      if (path instanceof pathClass)
+      if (path instanceof pathClass) {
+        this.beginPath();
         this.__draw__(path);
+      }
 
       this.__fill__();
     };
     kanvas2dContextProto.stroke = function (path) {
-      if (path instanceof pathClass)
+      if (path instanceof pathClass) {
+        this.beginPath();
         this.__draw__(path);
+      }
 
       this.__stroke__();
     };
     kanvas2dContextProto.clip = function (path) {
-      if (path instanceof pathClass)
+      if (path instanceof pathClass) {
+        this.beginPath();
         this.__draw__(path);
+      }
 
       this.__clip__();
     };
     kanvas2dContextProto.isPointInPath = function (x, y, path) {
-      if (path instanceof pathClass)
-        return;
+      if (path instanceof pathClass) {
+        this.beginPath();
+        this.__draw__(path);
+      }
 
       this.__isPointInPath__(x, y);
     };

commit c8f1fb6ef245876f11ef6f4cbfecf9d7b6b78975

Fixing regression caused by e1409cdd046dfc14da2683

src/swf/renderer.js
@@ -25,6 +25,7 @@ function renderDisplayObject(child, ctx, transform, cxform, clip) {
           ctx.fillStyle = path.fillStyle;
           if (path.fillTransform) {
             var m = path.fillTransform;
+            ctx.beginPath();
             ctx.__draw__(path);
             ctx.save();
             ctx.transform(m.a, m.b, m.c, m.d, m.tx, m.ty);