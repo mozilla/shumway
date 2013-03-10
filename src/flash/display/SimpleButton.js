8ee8d91b9233509ccf7662c1c6dbbf287ceb906e
Yury Delendik <ydelendik@mozilla.com>
2013-03-10 16:26:40 -0500

    Track buttons controls for sub-sprites

--- src/flash/display/SimpleButton.js
+++ src/flash/display/SimpleButton.js
@@ -97,7 +97,11 @@ var SimpleButtonDefinition = (function () {
         state = this._downState;
       else if (this._isMouseOver && this._overState)
         state = this._overState;
+      if (this._children.length > 0) {
+        this._control.removeChild(this._children[0]._control);
+      }
       this._children = [state];
+      this._control.appendChild(state._control);
 
       if (this._avm1MouseEvents) {
         this._processAvm1MouseEvents(this._avm1MouseEvents);
