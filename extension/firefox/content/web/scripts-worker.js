df37886e61509bae25c47e51577f8cab14a4bbdd
Yury Delendik <ydelendik@mozilla.com>
2013-04-11 11:50:17 -0700

    Fixes scrips-worker.js inclusions

--- extension/firefox/content/web/scripts-worker.js
+++ extension/firefox/content/web/scripts-worker.js
@@ -3,10 +3,12 @@
  */
 
 load($SHUMWAY_ROOT + "lib/DataView.js/DataView.js");
+load($SHUMWAY_ROOT + "src/flash/util.js");
 load($SHUMWAY_ROOT + "src/swf/util.js");
 load($SHUMWAY_ROOT + "src/swf/swf.js");
 load($SHUMWAY_ROOT + "src/swf/inflate.js");
 load($SHUMWAY_ROOT + "src/swf/stream.js");
+load($SHUMWAY_ROOT + "src/swf/templates.js");
 load($SHUMWAY_ROOT + "build/firefox/content/swf/handlers.js");
 load($SHUMWAY_ROOT + "src/swf/parser.js");
 load($SHUMWAY_ROOT + "src/swf/bitmap.js");
