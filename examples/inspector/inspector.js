809f6dce4fdd048e9bed213277588bceb78c3fb4
Yury Delendik <ydelendik@mozilla.com>
2013-04-18 17:24:31 -0500

    Moves unescape into the inspector

--- examples/inspector/inspector.js
+++ examples/inspector/inspector.js
@@ -142,7 +142,7 @@ function parseQueryString(qs) {
   for (var i = 0; i < values.length; i++) {
     var kv = values[i].split('=');
     var key = kv[0], value = kv[1];
-    obj[key] = value;
+    obj[key] = decodeUriComponent(value);
   }
 
   return obj;
