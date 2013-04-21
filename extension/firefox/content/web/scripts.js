3b388f019507e555a71e1890ed7e34e0e72efb0c
Yury Delendik <ydelendik@mozilla.com>
2013-04-20 23:01:05 -0500

    Adds hacks.js to extension

--- extension/firefox/content/web/scripts.js
+++ extension/firefox/content/web/scripts.js
@@ -67,6 +67,7 @@ load($SHUMWAY_ROOT + "src/avm2/compiler/c4/backend.js");
 load($SHUMWAY_ROOT + "src/avm2/domain.js");
 load($SHUMWAY_ROOT + "src/avm2/xregexp.js");
 load($SHUMWAY_ROOT + "src/avm2/runtime.js");
+load($SHUMWAY_ROOT + "src/avm2/hacks.js");
 load($SHUMWAY_ROOT + "src/avm2/xml.js");
 load($SHUMWAY_ROOT + "src/avm2/json2.js");
 load($SHUMWAY_ROOT + "src/avm2/amf.js");
