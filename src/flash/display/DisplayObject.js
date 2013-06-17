d82bb1e2e091d4b3b520a706faeaa8272a22bcd8
Tobias Schneider <schneider@jancona.com>
2013-06-17 13:00:18 +0200

    Simplify DisplayObject::width/height accessors.

--- src/flash/display/DisplayObject.js
+++ src/flash/display/DisplayObject.js
@@ -35,27 +35,6 @@ var DisplayObjectDefinition = (function () {
   var BLEND_MODE_SHADER     = 'shader';
   var BLEND_MODE_SUBTRACT   = 'subtract';
 
-  var ADJUST_SCALE_RATIO_TABLE = [0, 0.07973903588256626, 0.1500346500346502,
-    0.21160295103957072, 0.26651330923430827, 0.3182410423452769,
-    0.36618798955613585, 0.4117063492063492, 0.45627118644067816,
-    0.5, 0.5438401775804661, 0.5876777251184835, 0.6337907375643225,
-    0.6818830242510697, 0.7325392528424471, 0.7887683471601787,
-    0.850592885375494, 0.9202975557917109, 1];
-
-  function getAdjustScaleRatio(rotation) {
-    // black-box function, using linear interpolation
-    rotation = (180 + (rotation % 180)) % 180;
-    if (rotation === 0)
-      return 0;
-    var x = rotation > 90 ? (180 - rotation) / 90 : rotation / 90;
-    if (x >= 1)
-      return 1;
-    var i = (x * ADJUST_SCALE_RATIO_TABLE.length) | 0;
-    var ratio = x - (i / ADJUST_SCALE_RATIO_TABLE.length);
-    return ADJUST_SCALE_RATIO_TABLE[i] +
-      (ADJUST_SCALE_RATIO_TABLE[i + 1] - ADJUST_SCALE_RATIO_TABLE[i]) * ratio;
-  }
-
   var def = {
     __class__: 'flash.display.DisplayObject',
 
@@ -180,8 +159,8 @@ var DisplayObjectDefinition = (function () {
       }
       this._dispatchEvent(new flash.events.Event("addedToStage"));
     },
-    _applyCurrentInverseTransform: function (point, targetCoordSpace) {
-      if (this._parent && this._parent !== this._stage && this._parent !== targetCoordSpace)
+    _applyCurrentInverseTransform: function (point) {
+      if (this._parent && this._parent !== this._stage)
         this._parent._applyCurrentInverseTransform(point);
 
       var m = this._currentTransform;
@@ -192,9 +171,6 @@ var DisplayObjectDefinition = (function () {
       point.y = (m.a * y - m.b * x) * d;
     },
     _applyCurrentTransform: function (point, targetCoordSpace) {
-      if (targetCoordSpace === this)
-        return;
-
       var m = this._currentTransform;
       var x = point.x;
       var y = point.y;
@@ -202,8 +178,11 @@ var DisplayObjectDefinition = (function () {
       point.x = m.a * x + m.c * y + m.tx;
       point.y = m.d * y + m.b * x + m.ty;
 
-      if (this._parent && this._parent !== this._stage && this._parent !== targetCoordSpace)
-        this._parent._applyCurrentTransform(point, targetCoordSpace);
+      if (this._parent && this._parent !== this._stage)
+        this._parent._applyCurrentTransform(point);
+
+      if (targetCoordSpace)
+        targetCoordSpace._applyCurrentInverseTransform(point);
     },
     _hitTest: function (use_xy, x, y, useShape, hitTestObject, ignoreChildren) {
       if (use_xy) {
@@ -382,8 +361,9 @@ var DisplayObjectDefinition = (function () {
         return;
       }
 
-      var scaleRatio = getAdjustScaleRatio(this._rotation);
-      this.scaleX += scaleRatio * (this.scaleY - this.scaleX);
+      var baseWidth = u * (bounds.xMax - bounds.xMin) +
+                      v * (bounds.yMax - bounds.yMin);
+      this.scaleX = this.width / baseWidth;
 
       this.scaleY = val / baseHeight;
     },
@@ -529,12 +509,12 @@ var DisplayObjectDefinition = (function () {
         return;
       }
 
-      var scaleRatio = getAdjustScaleRatio(this._rotation);
-      this.scaleY += scaleRatio * (this.scaleX - this.scaleY);
+      var baseHeight = v * (bounds.xMax - bounds.xMin) +
+                       u * (bounds.yMax - bounds.yMin);
+      this.scaleY = this.height / baseHeight;
 
       this.scaleX = val / baseWidth;
     },
-    get x() {
       return this._x;
     },
     set x(val) {
