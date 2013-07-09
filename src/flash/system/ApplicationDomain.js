8dfcc66827d2221787a1ba8aa60335c9a9eacbe7
Michael Bebenita <mbebenita@mozilla.com>
2013-07-09 15:50:32 -0700

    Fix JSHint errors.

--- src/flash/system/ApplicationDomain.js
+++ src/flash/system/ApplicationDomain.js
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*global AVM2, Domain, Multiname */
 /**
  * Application domains don't have 1-1 object reference relationship with the real domain objects, not sure
  * why, but for instance ApplicationDomain.currentDomain !== ApplicationDomain.currentDomain. The getter
