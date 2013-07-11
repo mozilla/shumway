mozilla/shumway

commit e9c500a43c6d945f7738ebb12659b98aae1d335e

Move processing of timeline-created TextField contents into TextField, and implement rich text parsing and rendering based on DOM nodes and CSS styles.
Well, and cleanup and stuff.

Conflicts:
	src/flash/display/Loader.js
	src/flash/text/TextField.js
	src/swf/renderer.js

src/swf/text.js
@@ -18,48 +18,19 @@
 /*global toStringRgba */
 
 function defineText(tag, dictionary) {
-  var cmds = [];
-  cmds.push('c.save()');
-  cmds.push('c.beginPath()');
-  cmds.push('c.rect(' + tag.bbox.left + ', ' + tag.bbox.top + ', ' +
-                        (tag.bbox.right - tag.bbox.left) + ', ' +
-                        (tag.bbox.bottom - tag.bbox.top) + ')');
-  cmds.push('c.clip()');
-  cmds.push('c.scale(0.05, 0.05)');
   var dependencies = [];
-  var initialText;
-  var y;
-  if (tag.hasText) {
-    initialText = tag.html ? tag.initialText.replace(/<[^>]*>/g, '') : tag.initialText;
-  } else {
-    initialText = '';
-  }
-
   if (tag.hasFont) {
-    y = tag.fontHeight - tag.leading;
     var font = dictionary[tag.fontId];
     assert(font, 'undefined font', 'label');
-    cmds.push('c.font="' + tag.fontHeight + 'px \'' + font.name + '\'"');
     dependencies.push(font.id);
-  } else {
-    // height of 12pt in twips
-    y = 12 * (92 / 72) * 20;
-    cmds.push('c.font="' + y + 'px \'sans\'"');
   }
-  if (tag.hasColor)
-    cmds.push('c.fillStyle="' + toStringRgba(tag.color) + '"');
-  cmds.push('c.fillText(this.text,0,' + (y - 20 * tag.bbox.top) + ')');
 
-  cmds.push('c.restore();');
-  var text = {
+  var props = {
     type: 'text',
     id: tag.id,
-    bbox: tag.bbox,
-    variableName: tag.variableName,
-    value: initialText,
-    data: cmds.join('\n')
+    tag: tag
   };
   if (dependencies.length)
-    text.require = dependencies;
-  return text;
+    props.require = dependencies;
+  return props;
 }

src/flash/display/DisplayObject.js
@@ -258,9 +258,7 @@ var DisplayObjectDefinition = (function () {
       return width > 0 && height > 0;
     },
     _markAsDirty: function() {
-      if (!this._dirtyArea)
-        this._dirtyArea = this.getBounds();
-      this._bounds = null;
+      this._dirty = true;
     },
     _removedFromStage: function (e) {
       var children = this._children;

src/flash/display/Loader.js
@@ -875,22 +875,16 @@ var LoaderDefinition = (function () {
         props.height = symbol.height;
         break;
       case 'label':
-        var drawFn = new Function('d,c,r', symbol.data);
-        className = 'flash.text.StaticText';
-        props.bbox = symbol.bbox;
-        props.draw = function (c, r) {
-          return drawFn.call(this, dictionary, c, r);
-        };
-        break;
       case 'text':
-        var drawFn = new Function('d,c,r', symbol.data);
-        className = 'flash.text.TextField';
         props.bbox = symbol.bbox;
-        props.draw = function (c, r) {
-          return drawFn.call(this, dictionary, c, r);
-        };
-        props.text = symbol.value;
-        props.variableName = symbol.variableName;
+        props.html = symbol.html;
+        if (symbol.type === 'label') {
+          className = 'flash.text.StaticText';
+        } else {
+          className = 'flash.text.TextField';
+          props.tag = symbol.tag;
+          props.variableName = symbol.variableName;
+        }
         break;
       case 'shape':
         var createGraphicsSubPaths = new Function('c,d,r', 'return ' + symbol.data);

src/flash/text/TextField.js
@@ -18,6 +18,233 @@
 /*global AS2TextField */
 
 var TextFieldDefinition = (function () {
+
+  var xmlParser = new XMLParser();
+
+  /*
+   * Parsing "html", in this context, actually means ensuring that the given
+   * string is valid *xml*, and removing any tags that mustn't be supported.
+   *
+   * After that, two things are generated: a plain-text version of the content,
+   * and a list of ranges with types and attributes, representing all nested
+   * nodes.
+   */
+  function parseHtml(val) {
+    var xml = xmlParser.parseFromString(val).children[0];
+    var content = {ranges : [], text : ''};
+    convertXML(xml, content);
+    return content;
+  }
+
+  function convertXML(xml, content) {
+    // Ignore all comments, processing instructions and namespaced nodes.
+    if (xml.kind !== 'element' && xml.kind !== 'text' ||
+        xml.name.prefix !== '')
+    {
+      return;
+    }
+    var range = {start : content.text.length};
+    content.ranges.push(range);
+
+    if (xml.kind === 'text') {
+      var text = xml.value;
+      range.type = 'text';
+      range.text = text;
+      content.text += text;
+      range.end = content.text.length;
+      return;
+    }
+
+    range.type = xml.name.localName;
+    range.attributes = extractAttributes(xml);
+
+    var children = xml.children;
+    var childCount = children.length;
+    for (var i = 0; i < childCount; i++) {
+      convertXML(children[i], content);
+    }
+    range.end = content.text.length;
+  }
+
+  /**
+   * Creates an object containing all attributes with their localName as keys.
+   * Ignores all namespaced attributes, as we don't need them for the
+   * TextField's purposes.
+   * TODO: check whether we still have to keep them around for round-tripping.
+   */
+  function extractAttributes(node) {
+    var attributesList = node.attributes;
+    var attributesMap = Object.create(null);
+    for (var i = 0; i < attributesList.length; i++) {
+      var attr = attributesList[i];
+      if (attr.name.prefix !== '') {
+        continue;
+      }
+      attributesMap[attr.name.localName] = attr.value;
+    }
+    return attributesMap;
+  }
+
+  function renderRanges(ranges) {
+    var output = '';
+    var length = 0;
+    var rangeStack = [];
+    var currentRange = null;
+
+    for (var i = 0; i < ranges.length; i++) {
+      var range = ranges[i];
+      while (currentRange && currentRange.end <= range.start) {
+        output += renderRangeEnd(currentRange);
+        rangeStack.pop();
+        currentRange = rangeStack[rangeStack.length - 1];
+      }
+      assert(range.start === length);
+      if (range.type === 'text') {
+        output += range.text;
+        length = range.end;
+        continue;
+      }
+      output += renderRangeStart(range);
+      currentRange = range;
+      rangeStack.push(range);
+    }
+    while (rangeStack.length) {
+      output += renderRangeEnd(rangeStack.pop());
+    }
+    console.log(output);
+    return output;
+  }
+  function renderRangeStart(range) {
+    var attributes = range.attributes;
+    var output;
+    var styles;
+    switch (range.type) {
+      case 'br': return '<br />';
+      case 'b': return '<strong>';
+      case 'i': return '<i>';
+      // TODO: check if we need to emit <ul>'s around runs of <li>'s
+      case 'li': return '<li>';
+      case 'u': return '<span style="text-decoration: underline;">';
+      case 'a': {
+        output = '<a';
+        if ('href' in attributes) {
+          output += ' href="' + attributes.href + '"';
+        }
+        if ('target' in attributes) {
+          output += ' target="' + attributes.href + '"';
+        }
+        break;
+      }
+      case 'font': {
+        output = '<span';
+        styles = '';
+        if ('color' in attributes) {
+          styles += 'color:' + attributes.color + ';';
+        }
+        if ('face' in attributes) {
+          var fontFace = attributes.face;
+          if (fontFace === '_sans') {
+            fontFace = 'sans-serif';
+          } else if (fontFace === '_serif') {
+            fontFace = 'serif';
+          }
+          //TODO: adapt to embedded font names
+          styles += 'font-family:' + fontFace + ';';
+        }
+        if ('size' in attributes) {
+          //TODO: verify that px is the right unit
+          styles += 'font-size:' + attributes.size + 'px;';
+        }
+        break;
+      }
+      case 'img': {
+        output = '<img src="' + (attributes.src || '') + '"';
+        styles = '';
+        if ('width' in attributes) {
+          styles += 'width:' + attributes.width + 'px;';
+        }
+        if ('height' in attributes) {
+          styles += 'height:' + attributes.height + 'px;';
+        }
+        if ('hspace' in attributes && 'vspace' in attributes) {
+          styles += 'margin:' + attributes.vspace + 'px ' +
+                    attributes.hspace + 'px;';
+        } else if ('hspace' in attributes) {
+          styles += 'margin:0 ' + attributes.hspace + 'px;';
+        } else if ('vspace' in attributes) {
+          styles += 'margin:' + attributes.hspace + 'px 0;';
+        }
+        // TODO: support `align`, `id` and `checkPolicyFile`
+        output += ' /';
+        break;
+      }
+      case 'p': {
+        output = '<p';
+        styles = '';
+        if ('class' in attributes) {
+          styles += ' class="' + attributes['class'] + '"';
+        }
+        if ('align' in attributes) {
+          styles += 'text-align:' + attributes.align;
+        }
+        break;
+      }
+      case 'span': {
+        output = '<span';
+        if ('class' in attributes) {
+          output += ' class="' + attributes['class'] + '"';
+        }
+        break;
+      }
+      case 'textformat': {
+        // TODO: we probably need to merge textformat nodes with <p> nodes
+        output = '<span';
+        var styles = '';
+        var marginLeft = 0;
+        if ('blockIdent' in attributes) {
+          marginLeft += parseInt(attributes.blockIndent);
+        }
+        if ('leftMargin' in attributes) {
+          marginLeft += parseInt(attributes.leftMargin);
+        }
+        if (marginLeft !== 0) {
+          styles += 'margin-left:' + marginLeft + 'px"';
+        }
+        if ('indent' in attributes) {
+          styles += 'text-indent:' + attributes.indent + 'px"';
+        }
+        if ('rightMargin' in attributes) {
+          styles += 'margin-left:' + marginLeft + 'px"';
+        }
+        // TODO: support leading
+        // TODO: support tabStops, if possible
+        break;
+      }
+      default:
+        // For all unknown nodes, we just emit spans. We might not actually
+        // need to, but it doesn't do any harm, either.
+        return '<span>';
+    }
+    if (styles) {
+      output += ' style="' + styles + '"';
+    }
+    return output + '>';
+  }
+  function renderRangeEnd(range) {
+    switch (range.type) {
+      case 'b': return '</strong>';
+      case 'i':
+      case 'li':
+      case 'a':
+      case 'p': {
+        return '</' + range.type + '>';
+      }
+      default: { // <u>, <font>, <span>, <textformat>, and all others
+        return '</span>';
+      }
+    }
+  }
+
   var def = {
     __class__: 'flash.text.TextField',
 
@@ -26,7 +253,6 @@ var TextFieldDefinition = (function () {
       this._type = 'dynamic';
       this._textHeight = 0;
       this._textWidth = 0;
-      this._text = '';
       this._embedFonts = false;
 
       var bbox = this._bbox;
@@ -36,9 +262,26 @@ var TextFieldDefinition = (function () {
       }
 
       var s = this.symbol;
-      if (s) {
-        this.draw = s.draw || this.draw;
-        this._text = s.text || '';
+      if (!s) {
+        return;
+      }
+
+      var tag = s.tag;
+      var bbox = tag.bbox;
+      this._elementWidth = (bbox.right - bbox.left);
+      this._elementHeight = (bbox.bottom - bbox.top);
+      //TODO: we might need to move things down and right by 2px, or add a 2px margin
+      this.boundsStyle = 'top:' + bbox.top + 'px;left:' + bbox.left + 'px;' +
+                         'width:' + this._elementWidth + 'px;' +
+                         'height:' + this._elementHeight + 'px;';
+
+      if (tag.initialText) {
+        if (tag.html) {
+          this.htmlText = tag.initialText;
+        } else {
+          this.text = tag.initialText;
+        }
+        this._contentChanged = true;
       }
     },
 
@@ -54,34 +297,63 @@ var TextFieldDefinition = (function () {
       this._markAsDirty();
     },
 
-    draw: function (c) {
-      // TODO
+    draw: function (ctx) {
       var bbox = this._bbox;
-      if (!bbox) {
-        return;
-      }
-      c.save();
-      c.beginPath();
-      c.rect(bbox.left, bbox.top, bbox.right - bbox.left, bbox.bottom - bbox.top);
-      c.clip();
-      c.fillText(this.text, bbox.left, bbox.bottom);
-      c.restore();
+      var content = this._contentChanged === false ? this._renderedContent :
+          "<svg xmlns='http://www.w3.org/2000/svg' " +
+          "width='" + this._elementWidth +
+          "' height='" + this._elementHeight + "'>" +
+          "<foreignObject width='100%' height='100%'>" +
+          "<div xmlns='http://www.w3.org/1999/xhtml' style='" +
+          this.boundsStyle + "'>" + renderRanges(this._content.ranges) +
+          '</div></foreignObject></svg>';
+
+      this._renderedContent = content;
+      this._contentChanged = false;
+
+      var img = new Image();
+      var svg = new Blob([content], {type: "image/svg+xml;charset=utf-8"});
+      var DOMURL = self.URL || self.webkitURL || self;
+      var url = DOMURL.createObjectURL(svg);
+      img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+        DOMURL.revokeObjectURL(url);
+      };
+      img.src = url;
     },
 
     get text() {
       return this._text;
     },
     set text(val) {
-      if (this._text !== val) {
-        this._text = val;
-        this._markAsDirty();
+      if (this._text === val) {
+        return;
       }
+      //TODO: add default markup.
+      var range = {type : 'text', text: val, start : 0, end : val.length};
+      this._content = {ranges : [range], text : val};
+      this._htmlText = val;
+      this._contentChanged = true;
+      this._markAsDirty();
+    },
+
+    get htmlText() {
+      return this._htmlText;
+    },
+    set htmlText(val) {
+      if (this._htmlText === val) {
+        return;
+      }
+      this._htmlText = val;
+      this._content = parseHtml(val);
+      this._contentChanged = true;
+      this._markAsDirty();
     },
 
     get defaultTextFormat() {
       return this._defaultTextFormat;
     },
-    set	defaultTextFormat(val) {
+    set defaultTextFormat(val) {
       this._defaultTextFormat = val;
     },
 

commit 488b146aa0eb08a16c0d6c08c92c1affaeeaa6d0

Fixes AVM1/TextField regression from e9c500a4

src/swf/text.js
@@ -28,6 +28,7 @@ function defineText(tag, dictionary) {
   var props = {
     type: 'text',
     id: tag.id,
+    variableName: tag.variableName, // for AVM1
     tag: tag
   };
   if (dependencies.length)