faa52f32d02e0ee75c20c2036ef7889c0eb00721
Michael Bebenita <mbebenita@mozilla.com>
2013-07-26 01:19:09 -0700

    Fix lint errors.

--- src/flash/display/Bitmap.js
+++ src/flash/display/Bitmap.js
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*global FrameCounter */
+/*global FrameCounter, traceRenderer, frameWriter */
 
 var BitmapDefinition = (function () {
   function setBitmapData(value) {
