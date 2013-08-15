mozilla/shumway

commit f8fda4f7ab2fe364cc89b899f2be58559590f022

Directs AS2 calls to public AVM2 methods

src/avm1/classes.js
@@ -20,15 +20,15 @@
 
 function proxyNativeProperty(propertyName) {
   return {
-    get: function getter() { return this.$nativeObject[propertyName]; },
-    set: function setter(value) { this.$nativeObject[propertyName] = value; },
+    get: function getter() { return this.$nativeObject.getMultinameProperty(undefined, propertyName, 0); },
+    set: function setter(value) { this.$nativeObject.setMultinameProperty(undefined, propertyName, 0, value); },
     enumerable: true
   };
 }
 
 function proxyNativeReadonlyProperty(propertyName) {
   return {
-    get: function getter() { return this.$nativeObject[propertyName]; },
+    get: function getter() { return this.$nativeObject.getMultinameProperty(undefined, propertyName, 0); },
     enumerable: true
   };
 }
@@ -36,7 +36,8 @@ function proxyNativeReadonlyProperty(propertyName) {
 function proxyNativeMethod(methodName) {
   return {
     value: function proxyMethod(id) {
-      return this.$nativeObject[methodName].apply(this.$nativeObject, arguments);
+      return this.$nativeObject.callMultinameProperty(undefined, methodName,
+        0, true, arguments);
     },
     enumerable: true
   };

commit 35ad102f15e822e3fb364fc6c2806d557cbad4ea

Fixes regression in xiao3 caused by f8fda4f7ab2

src/avm1/classes.js
@@ -276,7 +276,7 @@ AS2MovieClip.prototype = Object.create(Object.prototype, {
     set: function set$forceSmoothing(value) { throw 'Not implemented: set$forceSmoothing'; },
     enumerable: true
   },
-  _framesloaded: proxyNativeReadonlyProperty('_framesLoaded'),
+  _framesloaded: proxyNativeReadonlyProperty('framesLoaded'),
   getBounds: {
     value: function getBounds(bounds) {
       var obj = bounds.$nativeObject;