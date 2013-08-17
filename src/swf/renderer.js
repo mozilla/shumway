cee04cefdd59cc923a4038d062bf1049839a1327
Claus Wahlers <claus@codeazur.com.br>
2013-08-16 23:03:35 -0300

    better explanation of TODOs

--- src/swf/renderer.js
+++ src/swf/renderer.js
@@ -241,36 +241,34 @@ function renderDisplayObject(child, ctx, transform, cxform, clip) {
 
   var blendModeClass = flash.display.BlendMode.class;
   if (child._blendMode !== blendModeClass.NORMAL) {
+
     // TODO:
-    // blendModeClass.LAYER
-    // blendModeClass.SUBTRACT
-    // blendModeClass.INVERT
-    // blendModeClass.SHADER
-    // blendModeClass.ADD (lighter)
-    // blendModeClass.ALPHA (destination-in)
-    // blendModeClass.ERASE (destination-out)
+
+    // These Flash blend modes have no canvas equivalent:
+    // - blendModeClass.SUBTRACT
+    // - blendModeClass.INVERT
+    // - blendModeClass.SHADER
+    // - blendModeClass.ADD
+
+    // These blend modes are actually Porter-Duff compositing operators.
+    // The backdrop is the nearest parent with blendMode set to LAYER.
+    // When there is no LAYER parent, they are ignored (treated as NORMAL).
+    // - blendModeClass.ALPHA (destination-in)
+    // - blendModeClass.ERASE (destination-out)
+    // - blendModeClass.LAYER [defines backdrop]
+
+    var blendModeCanvas;
     switch (child._blendMode) {
-      case blendModeClass.MULTIPLY:
-        ctx.globalCompositeOperation = "multiply";
-        break;
-      case blendModeClass.SCREEN:
-        ctx.globalCompositeOperation = "screen";
-        break;
-      case blendModeClass.LIGHTEN:
-        ctx.globalCompositeOperation = "lighten";
-        break;
-      case blendModeClass.DARKEN:
-        ctx.globalCompositeOperation = "darken";
-        break;
-      case blendModeClass.DIFFERENCE:
-        ctx.globalCompositeOperation = "difference";
-        break;
-      case blendModeClass.OVERLAY:
-        ctx.globalCompositeOperation = "overlay";
-        break;
-      case blendModeClass.HARDLIGHT:
-        ctx.globalCompositeOperation = "hard-light";
-        break;
+      case blendModeClass.MULTIPLY:   blendModeCanvas = "multiply";        break;
+      case blendModeClass.SCREEN:     blendModeCanvas = "screen";          break;
+      case blendModeClass.LIGHTEN:    blendModeCanvas = "lighten";         break;
+      case blendModeClass.DARKEN:     blendModeCanvas = "darken";          break;
+      case blendModeClass.DIFFERENCE: blendModeCanvas = "difference";      break;
+      case blendModeClass.OVERLAY:    blendModeCanvas = "overlay";         break;
+      case blendModeClass.HARDLIGHT:  blendModeCanvas = "hard-light";      break;
+    }
+    if (blendModeCanvas !== undefined) {
+      ctx.globalCompositeOperation = blendModeCanvas;
     }
   }
 
