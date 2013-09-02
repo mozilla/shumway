af54fd55d192e3947f34d96fde3bac22838559e4
Claus Wahlers <claus@codeazur.com.br>
2013-09-02 01:10:48 -0300

    shows current/total frame for movieclips; selectable items

--- examples/inspector/js/classes/DisplayListTree.js
+++ examples/inspector/js/classes/DisplayListTree.js
@@ -17,36 +17,66 @@
  */
 var DisplayListTree = (function() {
 
+  var rootElement;
+  var hoveredElement;
+  var selectedElement;
+  var boundClickListener;
+  var boundMouseOverListener;
+  var displayObjectStore;
+
   function processChildren(parent) {
     var children = parent.displayObject._children;
     for (var i = 0, n = children.length; i < n; i++) {
       var child = children[i];
       if (child) {
-        var item = { displayObject: child, children: [] };
-        var isContainer = flash.display.DisplayObjectContainer.class.isInstanceOf(child) ||
-                          flash.display.SimpleButton.class.isInstanceOf(child);
-        if (isContainer) {
+        var item = {
+          displayObject: child,
+          children: [],
+          parent: parent,
+          index: displayObjectStore.length,
+          hasTimeline: flash.display.MovieClip.class.isInstanceOf(child)
+        };
+        parent.children.push(item);
+        displayObjectStore.push(item);
+        if (flash.display.DisplayObjectContainer.class.isInstanceOf(child) || flash.display.SimpleButton.class.isInstanceOf(child)) {
           processChildren(item);
         }
-        parent.children.push(item);
-        //console.log(item.displayObject.class.className, item.displayObject.__class__);
       }
     }
   }
 
+  function findItemElement(el) {
+    while (el && el !== rootElement) {
+      if (el.classList.contains("item")) {
+        return el;
+      }
+      el = el.parentElement;
+    }
+    return null;
+  }
+
   var ctor = function(root) {
-    this.root = { displayObject: root, children: [] };
+    this.root = { displayObject: root, children: [], parent: null, index: 0 };
+    displayObjectStore = [ this.root ];
     processChildren(this.root);
   }
 
   ctor.prototype = {
 
     updateDom: function updateDom(elRoot) {
+      var that = this;
       function updateChildren(elContainer, item) {
         var li = document.createElement("li");
-        var span = document.createElement("span");
-        span.textContent = item.displayObject.class.className;
-        li.appendChild(span);
+        var div = document.createElement("div");
+        div.textContent = item.displayObject.class.className + " ";
+        if (item.hasTimeline) {
+          var span = document.createElement("span");
+          span.textContent = item.displayObject._currentFrame + "/" + item.displayObject._totalFrames;
+          div.appendChild(span);
+        }
+        div.className = "item";
+        div.dataset.dosidx = item.index;
+        li.appendChild(div);
         elContainer.appendChild(li);
         if (item.children.length > 0) {
           var ul = document.createElement("ul");
@@ -56,8 +86,54 @@ var DisplayListTree = (function() {
           li.appendChild(ul);
         }
       }
+      if (boundClickListener) {
+        elRoot.removeEventListener("click", boundClickListener);
+      }
+      if (boundMouseOverListener) {
+        elRoot.removeEventListener("mouseover", boundMouseOverListener);
+      }
+      boundClickListener = this._onClick.bind(this);
+      boundMouseOverListener = this._onMouseOver.bind(this);
+      elRoot.addEventListener("click", boundClickListener);
+      elRoot.addEventListener("mouseover", boundMouseOverListener);
       elRoot.innerHTML = '<ul id="displayListRoot"></ul>';
+      rootElement = elRoot;
       updateChildren(document.getElementById("displayListRoot"), this.root);
+    },
+
+    _onClick: function _onClick(event) {
+      function clearSelectedState() {
+        if (selectedElement) {
+          selectedElement.classList.remove("selected");
+        }
+      }
+      var el = findItemElement(event.target);
+      if (el) {
+        // CLICK
+        clearSelectedState();
+        if (el !== selectedElement || (selectedElement && !event.metaKey && !event.altKey)) {
+          // SELECT
+          selectedElement = el;
+          selectedElement.classList.add("selected");
+          //console.log(displayObjectStore[el.dataset.dosidx].displayObject.name)
+        } else if (selectedElement) {
+          // UNSELECT
+          selectedElement = null;
+        }
+      }
+    },
+
+    _onMouseOver: function _onMouseOver(event) {
+      var el = findItemElement(event.target);
+      if (el) {
+        if (el !== hoveredElement) {
+          // OVER
+          hoveredElement = el;
+        }
+      } else {
+        // OUT
+        hoveredElement = null;
+      }
     }
 
   };
