69a2be6fc1e0d03b497243257cd904871c6d3f76
Michael Bebenita <mbebenita@mozilla.com>
2013-09-05 18:10:39 -0700

    Add AOT stress testing to AVM shell.

---
+++ src/avm2/compiler/aot.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2013 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Throw away code, just used to debug the compiler for now.
+ */
+
+function compileScript(script, writer) {
+  script.traits.forEach(function (trait) {
+    if (trait.isClass()) {
+      compileClass(trait.classInfo, writer);
+    }
+  });
+}
+
+function compileClass(classInfo, writer) {
+  function compileTrait(trait) {
+    var traitName = Multiname.getQualifiedName(trait.name);
+    if (trait.isMethod() || trait.isGetter() || trait.isSetter()) {
+      var methodInfo = trait.methodInfo;
+      if (shouldCompile(methodInfo)) {
+        ensureFunctionIsInitialized(methodInfo);
+        try {
+          var method = createCompiledFunction(methodInfo, new Scope(null, {}), false, false, false);
+          if (trait.isMethod()) {
+            writer.writeLn("get_" + traitName + ": function() { return this." + VM_OPEN_METHOD_PREFIX + traitName + ".bind(this); },");
+          }
+          if (trait.isMethod()) {
+            writer.enter(VM_OPEN_METHOD_PREFIX + traitName + ": ");
+          } else if (trait.isGetter()) {
+            writer.enter("get_" + traitName + ": ");
+          } else if (trait.isSetter()) {
+            writer.enter("set_" + traitName + ": ");
+          }
+          writer.writeLns(method.toSource());
+          writer.leave(",");
+        } catch (x) {
+
+        }
+      }
+    }
+  }
+
+  function compileTraits(traits) {
+    traits.forEach(compileTrait);
+  }
+
+  function compileInitializer(methodInfo) {
+    if (canCompile(methodInfo)) {
+      ensureFunctionIsInitialized(methodInfo);
+      var method = createCompiledFunction(methodInfo, new Scope(null, {}), false, false, false);
+      writer.enter("constructor:");
+      writer.writeLns(method.toSource());
+      writer.leave(", ");
+    }
+  }
+
+  writer.enter(Multiname.getQualifiedName(classInfo.instanceInfo.name) + ": {");
+
+  writer.enter("static: {");
+  compileInitializer(classInfo.init);
+  compileTraits(classInfo.traits);
+  writer.leave("}, ");
+  writer.enter("instance: {");
+  compileInitializer(classInfo.instanceInfo.init);
+  compileTraits(classInfo.instanceInfo.traits);
+  writer.leave("}");
+  writer.leave("},");
+}
