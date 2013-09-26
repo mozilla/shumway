59f13ed53ca0e7e2416432196de2636c729cbc9f
Tobias Schneider <schneider@jancona.com>
2013-09-26 17:07:46 +0200

    Fix QuadTree subdivision.

--- src/flash/util.js
+++ src/flash/util.js
@@ -225,8 +225,8 @@ var QuadTree = function (x, y, width, height, level) {
   this.nodes = [];
 };
 QuadTree.prototype._findIndex = function (xMin, yMin, xMax, yMax) {
-  var midX = this.x + (this.width / 2);
-  var midY = this.y + (this.height / 2);
+  var midX = this.x + ((this.width / 2) | 0);
+  var midY = this.y + ((this.height / 2) | 0);
 
   var top = yMin < midY && yMax < midY;
   var bottom = yMin > midY;
@@ -322,15 +322,13 @@ QuadTree.prototype.retrieve = function (xMin, yMin, xMax, yMax) {
   return out;
 };
 QuadTree.prototype._subdivide = function () {
-  var widthLeft = this.width / 2;
-  var widthRight = this.width - widthLeft;
-  var heightTop = this.height / 2;
-  var heightBottom = this.height - heightTop;
-  var midX = this.x + widthLeft;
-  var midY = this.y + heightTop;
+  var halfWidth = (this.width / 2) | 0;
+  var halfHeight = (this.height / 2) | 0;
+  var midX = this.x + halfWidth;
+  var midY = this.y + halfHeight;
   var level = this.level + 1;
-  this.nodes[0] = new QuadTree(this.x, this.y, widthLeft, heightTop, level);
-  this.nodes[1] = new QuadTree(midX, this.y, widthRight, heightTop, level);
-  this.nodes[2] = new QuadTree(this.x, midY, widthLeft, heightBottom, level);
-  this.nodes[3] = new QuadTree(midX, midY, widthRight, heightBottom, level);
+  this.nodes[0] = new QuadTree(midX, this.y, halfWidth, halfHeight, level);
+  this.nodes[1] = new QuadTree(this.x, this.y, halfWidth, halfHeight, level);
+  this.nodes[2] = new QuadTree(this.x, midY, halfWidth, halfHeight, level);
+  this.nodes[3] = new QuadTree(midX, midY, halfWidth, halfHeight, level);
 };
