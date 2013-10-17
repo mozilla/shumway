a9474b102cb9f514337e9984a475ed8856b475f0
Till Schneidereit <till@tillschneidereit.net>
2013-10-17 16:26:44 +0200

    Fix linter errors in TextField

--- src/flash/text/TextField.js
+++ src/flash/text/TextField.js
@@ -15,7 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*global avm1lib, rgbaObjToStr, rgbIntAlphaToStr, warning, FontDefinition */
+/*global avm1lib, rgbaObjToStr, rgbIntAlphaToStr, warning, FontDefinition,
+  Errors, throwError */
 
 var TextFieldDefinition = (function () {
 
