c206e1f2115df94a2651dd0f69ad47f49f45354e
Claus Wahlers <claus@codeazur.com.br>
2013-11-13 03:36:25 -0200

    some minor optimizations

--- examples/filter/asm.js/filters.post.js
+++ examples/filter/asm.js/filters.post.js
@@ -1,28 +1,32 @@
+
 var _preMultiplyAlpha = Module.cwrap('preMultiplyAlpha', null, ['number', 'number', 'number']);
 var _preMultiplyAlphaUndo = Module.cwrap('preMultiplyAlphaUndo', null, ['number', 'number', 'number']);
 var _blur = Module.cwrap('blur', null, ['number', 'number', 'number', 'number', 'number', 'number']);
 
 function preMultiplyAlpha(img, width, height) {
-  var pimg = Module._malloc(width * height * 4);
+  var size = width * height * 4;
+  var pimg = Module._malloc(size);
   Module.HEAPU8.set(img, pimg);
   _preMultiplyAlpha(pimg, width, height);
-  img.set(Module.HEAPU8.subarray(pimg, pimg + width * height * 4));
+  img.set(Module.HEAPU8.subarray(pimg, pimg + size));
   Module._free(pimg);
 }
 
 function preMultiplyAlphaUndo(img, width, height) {
-  var pimg = Module._malloc(width * height * 4);
+  var size = width * height * 4;
+  var pimg = Module._malloc(size);
   Module.HEAPU8.set(img, pimg);
   _preMultiplyAlphaUndo(pimg, width, height);
-  img.set(Module.HEAPU8.subarray(pimg, pimg + width * height * 4));
+  img.set(Module.HEAPU8.subarray(pimg, pimg + size));
   Module._free(pimg);
 }
 
 function blur(img, width, height, blurX, blurY, quality) {
   quality = quality || 1;
-  var pimg = Module._malloc(width * height * 4);
+  var size = width * height * 4;
+  var pimg = Module._malloc(size);
   Module.HEAPU8.set(img, pimg);
   _blur(pimg, width, height, blurX, blurY, quality);
-  img.set(Module.HEAPU8.subarray(pimg, pimg + width * height * 4));
+  img.set(Module.HEAPU8.subarray(pimg, pimg + size));
   Module._free(pimg);
 }
