184233398c407688855467188ce6cb5beee9a026
Claus Wahlers <claus@codeazur.com.br>
2013-11-13 17:05:01 -0200

    wrap lib to prevent global var cluttering

--- examples/filter/asm.js/filters.post.js
+++ examples/filter/asm.js/filters.post.js
@@ -1,29 +1,37 @@
 
-var _preMultiplyAlpha = Module.cwrap('preMultiplyAlpha', null, ['number', 'number', 'number']);
-var _preMultiplyAlphaUndo = Module.cwrap('preMultiplyAlphaUndo', null, ['number', 'number', 'number']);
-var _blur = Module.cwrap('blur', null, ['number', 'number', 'number', 'number', 'number', 'number']);
+  var _preMultiplyAlpha = Module.cwrap('preMultiplyAlpha', null, ['number', 'number', 'number']);
+  var _preMultiplyAlphaUndo = Module.cwrap('preMultiplyAlphaUndo', null, ['number', 'number', 'number']);
+  var _blur = Module.cwrap('blur', null, ['number', 'number', 'number', 'number', 'number', 'number']);
 
-function preMultiplyAlpha(img, width, height) {
-  var pimg = Module._malloc(img.length);
-  Module.HEAPU8.set(img, pimg);
-  _preMultiplyAlpha(pimg, width, height);
-  img.set(Module.HEAPU8.subarray(pimg, pimg + img.length));
-  Module._free(pimg);
-}
+  function preMultiplyAlpha(img, width, height) {
+    var pimg = Module._malloc(img.length);
+    Module.HEAPU8.set(img, pimg);
+    _preMultiplyAlpha(pimg, width, height);
+    img.set(Module.HEAPU8.subarray(pimg, pimg + img.length));
+    Module._free(pimg);
+  }
 
-function preMultiplyAlphaUndo(img, width, height) {
-  var pimg = Module._malloc(img.length);
-  Module.HEAPU8.set(img, pimg);
-  _preMultiplyAlphaUndo(pimg, width, height);
-  img.set(Module.HEAPU8.subarray(pimg, pimg + img.length));
-  Module._free(pimg);
-}
+  function preMultiplyAlphaUndo(img, width, height) {
+    var pimg = Module._malloc(img.length);
+    Module.HEAPU8.set(img, pimg);
+    _preMultiplyAlphaUndo(pimg, width, height);
+    img.set(Module.HEAPU8.subarray(pimg, pimg + img.length));
+    Module._free(pimg);
+  }
 
-function blur(img, width, height, blurX, blurY, quality) {
-  quality = quality || 1;
-  var pimg = Module._malloc(img.length);
-  Module.HEAPU8.set(img, pimg);
-  _blur(pimg, width, height, blurX, blurY, quality);
-  img.set(Module.HEAPU8.subarray(pimg, pimg + img.length));
-  Module._free(pimg);
-}
+  function blur(img, width, height, blurX, blurY, quality) {
+    quality = quality || 1;
+    var pimg = Module._malloc(img.length);
+    Module.HEAPU8.set(img, pimg);
+    _blur(pimg, width, height, blurX, blurY, quality);
+    img.set(Module.HEAPU8.subarray(pimg, pimg + img.length));
+    Module._free(pimg);
+  }
+
+  return {
+    preMultiplyAlpha: preMultiplyAlpha,
+    preMultiplyAlphaUndo: preMultiplyAlphaUndo,
+    blur: blur
+  };
+
+})();
