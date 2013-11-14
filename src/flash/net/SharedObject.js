407163d1da1ca1e645fef324ab5430f750c11d03
Tobias Schneider <schneider@jancona.com>
2013-11-14 16:57:15 +0100

    Fix lint error.

--- src/flash/net/SharedObject.js
+++ src/flash/net/SharedObject.js
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*global createEmptyObject, Multiname, TelemetryService, SHAREDOBJECT_FEATURE */
+/*global createEmptyObject, Multiname, TelemetryService, SHAREDOBJECT_FEATURE, sessionStorage */
 
 var SharedObjectDefinition = (function () {
 
