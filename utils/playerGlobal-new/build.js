2d0f92b3ef52444d4035948d726aa554e4daf552
Yury Delendik <ydelendik@mozilla.com>
2014-02-06 15:05:15 -0600

    Adds base classes to the dependencies

--- utils/playerGlobal-new/build.js
+++ utils/playerGlobal-new/build.js
@@ -1,3 +1,21 @@
+/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+/*
+ * Copyright 2014 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
@@ -134,6 +152,19 @@ function getDependencies(files) {
         } else if (!tmpErr[m[0]]) {
           console.log('Dependency file for \"' + m[0] + '\" was not found');
           tmpErr[m[0]] = true;
+        }
+      }
+      m = /\bclass\s+\w+([^{]+)/.exec(content);
+      if (m) {
+        var baseClasses = m[1].replace(/\b(extends|implements)\b/g, '');
+        re = /(\w+)/g;
+        while ((m = re.exec(baseClasses))) {
+          var path = file.substring(0, file.lastIndexOf('/') + 1) + m[1] + '.as';
+          if (fs.existsSync(path)) {
+            tmp[path] = true;
+            // uncomment to do recursive search of dependencies
+            // queue.push(path);
+          }
         }
       }
     } catch (e) {
