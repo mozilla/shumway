b716733f67d5b3b191bdb6bbac7b8d9d6fbc54b7
Claus Wahlers <claus@codeazur.com.br>
2014-03-14 16:49:08 -0300

    move global.ts and utilities.ts to src/

--- src/utilities.ts
+++ src/utilities.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-///<reference path='references.ts' />
+///<reference path='avm2/references.ts' />
 
 interface String {
   padRight(c: string, n: number): string;
