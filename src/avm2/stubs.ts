9ea53e2b5071ea2fd20687a999636e3518686932
Tobias Schneider <schneider@jancona.com>
2014-04-04 16:29:44 -0700

    Fix typo in license text

--- src/avm2/stubs.ts
+++ src/avm2/stubs.ts
@@ -71,7 +71,7 @@ module Shumway.AVM2 {
       "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
       "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
       "See the License for the specific language governing permissions and\n" +
-      "limitations undxr the License."
+      "limitations under the License."
     );
   }
 
