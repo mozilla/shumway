b05d05ed7697dfe10a53a72cd7eaba7add87502e
Till Schneidereit <till@tillschneidereit.net>
2014-04-10 16:12:06 +0200

    Rough TS port of StaticText class

--- src/flash.ts/text/StaticText.ts
+++ src/flash.ts/text/StaticText.ts
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Mozilla Foundation
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,35 +15,24 @@
  */
 // Class: StaticText
 module Shumway.AVM2.AS.flash.text {
-  import notImplemented = Shumway.Debug.notImplemented;
   export class StaticText extends flash.display.DisplayObject {
-    
-    // Called whenever the class is initialized.
+
     static classInitializer: any = null;
-    
-    // Called whenever an instance of the class is initialized.
     static initializer: any = null;
-    
-    // List of static symbols to link.
-    static staticBindings: string [] = null; // [];
-    
-    // List of instance symbols to link.
-    static bindings: string [] = null; // [];
-    
-    constructor () {
-      false && super();
-      notImplemented("Dummy Constructor: public flash.text.StaticText");
+    static staticBindings: string [] = null;
+    static bindings: string [] = null;
+
+    constructor() {
     }
-    
-    // JS -> AS Bindings
-    
-    
+
+    private _text: string;
+    set text(text: String): void {
+      this._text = text;
+    }
+
     // AS -> JS Bindings
-    
-    // _text: string;
     get text(): string {
-      notImplemented("public flash.text.StaticText::get text"); return;
-      // return this._text;
+      return this._text;
     }
   }
 }
