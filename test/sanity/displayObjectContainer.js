5eb520e6791433bad22a511508e79c89857ce91d
Michael Bebenita <mbebenita@gmail.com>
2014-04-18 12:55:28 -0700

    Some tests.

---
+++ test/sanity/displayObjectContainer.js
@@ -0,0 +1,27 @@
+(function displayTests() {
+
+  var Random = Shumway.Random;
+  var Rectangle = flash.geom.Rectangle;
+  var Event = flash.events.Event;
+  var DisplayObject = flash.display.DisplayObject;
+  var DisplayObjectFlags = flash.display.DisplayObjectFlags;
+  var InteractiveObject = flash.display.InteractiveObject;
+  var DisplayObjectContainer = flash.display.DisplayObjectContainer;
+  var Shape = flash.display.Shape;
+  var Sprite = flash.display.Sprite;
+
+  sanityTests.push(function (console) {
+    Random.seed(0x12343);
+
+    var s = new Shape();
+    var c = new DisplayObjectContainer();
+
+    var addedEventWasTriggered = false;
+    s.addEventListener(Event.ADDED, function () {
+      addedEventWasTriggered = true;
+    });
+    c.addChild(s);
+    check(addedEventWasTriggered);
+  });
+
+})();
---
+++ test/sanity/loader.js
@@ -0,0 +1,56 @@
+(function displayTests() {
+  var Event = flash.events.Event;
+  var Stage = flash.display.Stage;
+  var Loader = flash.display.Loader;
+  var URLRequest = flash.net.URLRequest;
+  var DisplayObject = flash.display.DisplayObject;
+  var DisplayObjectContainer = flash.display.DisplayObjectContainer;
+
+  function log(message) {
+    console.info(message);
+  }
+
+  sanityTests.push(function runInspectorSanityTests(console) {
+    var r = new URLRequest("../as3_tiger/tiger.swf");
+    var l = new Loader();
+    var s = new Stage();
+
+    var initEventCalled = false;
+    var completeEventCalled = false;
+
+    l.contentLoaderInfo.addEventListener(Event.INIT, function (event) {
+      initEventCalled = true;
+    });
+
+    l.contentLoaderInfo.addEventListener(Event.COMPLETE, function (event) {
+      completeEventCalled = true;
+    });
+
+    l.load(r);
+
+    check(initEventCalled, "Check contentLoaderInfo initEventCalled");
+    check(completeEventCalled, "Check contentLoaderInfo completeEventCalled");
+  });
+
+  sanityTests.push(function runInspectorSanityTests(console) {
+    var r = new URLRequest("../as3_tiger/tiger.swf");
+    var l = new Loader();
+    var s = new Stage();
+
+    l.contentLoaderInfo.addEventListener(Event.INIT, function (event) {
+      check(l.content, "We should have some content here.");
+      check(l.content.root === l.content, "This should be the root.");
+      check(DisplayObject.isType(l.content));
+      s.stageWidth = l.contentLoaderInfo.width;
+      s.stageHeight = l.contentLoaderInfo.height;
+      s.frameRate = l.contentLoaderInfo.frameRate;
+      s.addChild(l.content);
+      check(s.frameRate);
+    });
+
+    l.load(r);
+
+
+  });
+
+})();
