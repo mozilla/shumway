dcfbfbd47f3e55810942ac1e112b98dab1e54c41
Till Schneidereit <till@tillschneidereit.net>
2014-04-22 12:06:16 +0200

    Update copyright year in .ts files to 2014

--- src/avm1lib/AS2TextField.ts
+++ src/avm1lib/AS2TextField.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Mozilla Foundation
+ * Copyright 2014 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
