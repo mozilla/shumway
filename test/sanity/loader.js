b9af6bc845dc60983bcb9aaf044083843e2cbff9
Michael Bebenita <mbebenita@gmail.com>
2014-04-22 13:30:21 -0700

    Add stress test.

--- test/sanity/loader.js
+++ test/sanity/loader.js
@@ -10,45 +10,47 @@
     console.info(message);
   }
 
-  unitTests.push(function runInspectorSanityTests() {
-    return new Promise(function (resolve, reject) {
-      var r = new URLRequest("../as3_tiger/tiger.swf");
-      var l = new Loader();
-      var s = new Stage();
+  for (var i = 0; i < 75; i++) {
+    unitTests.push(function runInspectorSanityTests() {
+      return new Promise(function (resolve, reject) {
+        var r = new URLRequest("../as3_tiger/tiger.swf");
+        var l = new Loader();
+        var s = new Stage();
 
-      var initEventCalled = false;
-      var completeEventCalled = false;
-      var initEventPromise = new Promise(function (resolve) {
-        l.contentLoaderInfo.addEventListener(Event.INIT, function (event) {
-          initEventCalled = true;
-          resolve();
+        var initEventCalled = false;
+        var completeEventCalled = false;
+        var initEventPromise = new Promise(function (resolve) {
+          l.contentLoaderInfo.addEventListener(Event.INIT, function (event) {
+            initEventCalled = true;
+            resolve();
+          });
         });
-      });
 
-      var completeEventPromise = new Promise(function (resolve) {
-        l.contentLoaderInfo.addEventListener(Event.COMPLETE, function (event) {
-          completeEventCalled = true;
-          resolve();
+        var completeEventPromise = new Promise(function (resolve) {
+          l.contentLoaderInfo.addEventListener(Event.COMPLETE, function (event) {
+            completeEventCalled = true;
+            resolve();
+          });
         });
-      });
 
-      l.load(r);
+        l.load(r);
 
-      var checkEvents = function () {
-        check(initEventCalled, "Check contentLoaderInfo initEventCalled");
-        check(completeEventCalled, "Check contentLoaderInfo completeEventCalled");
-      };
+        var checkEvents = function () {
+          check(initEventCalled, "Check contentLoaderInfo initEventCalled");
+          check(completeEventCalled, "Check contentLoaderInfo completeEventCalled");
+        };
 
-      var timeout = setTimeout(function () {
-        reject('timeout');
-      }, 1000);
+        var timeout = setTimeout(function () {
+          reject('timeout');
+        }, 1000);
 
-      Promise.all([initEventPromise, completeEventPromise]).then(function () {
-        clearTimeout(timeout);
-        checkEvents();
+        Promise.all([initEventPromise, completeEventPromise]).then(function () {
+          clearTimeout(timeout);
+          checkEvents();
+        }).then(resolve, reject);
       });
     });
-  });
+  }
 
   unitTests.push(function runInspectorSanityTests() {
     var r = new URLRequest("../as3_tiger/tiger.swf");
