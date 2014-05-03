c492119007db56496f127bea21b21b8761a5e6ab
Michael Bebenita <mbebenita@gmail.com>
2014-05-02 22:44:10 -0700

    Some test cases.

--- test/sanity/displayObjectContainer.js
+++ test/sanity/displayObjectContainer.js
@@ -41,4 +41,81 @@
     check(addedToStageEventWasTriggered);
   });
 
+  unitTests.push(0, function () {
+    Random.seed(0x12343);
+    var stage = new Stage();
+    var s = new Shape();
+    var c = new DisplayObjectContainer();
+    var r = "";
+    var addedToStageEventWasTriggered = false;
+    s.addEventListener(Event.ADDED_TO_STAGE, function () {
+      r += "A";
+    });
+    c.addChild(s);
+    stage.addChild(c);
+    stage.addChild(c);
+    eq(r, "A");
+  });
+
+  unitTests.push(function () {
+    Random.seed(0x12343);
+    var stage = new Stage();
+    var s = new Shape();
+    var c = new DisplayObjectContainer();
+    var r = "";
+    var addedToStageEventWasTriggered = false;
+    s.addEventListener(Event.ADDED_TO_STAGE, function () {
+      r += "A";
+    });
+    s.addEventListener(Event.REMOVED_FROM_STAGE, function () {
+      r += "R";
+    });
+    c.addChild(s);
+    stage.addChild(c); // += "A"
+    c.removeChild(s); // += "R"
+    c.addChild(s); // += "A"
+    c.removeChild(s); // += "R"
+    c.addChild(s); // += "A"
+    eq(r, "ARARA");
+  });
+
+  unitTests.push(function () {
+    Random.seed(0x12343);
+    var stage = new Stage();
+    var s = new Shape();
+    var r = "";
+    s.addEventListener(Event.ADDED_TO_STAGE, function () {
+      r += "A";
+    });
+    s.addEventListener(Event.REMOVED_FROM_STAGE, function () {
+      r += "R";
+    });
+    var c = new DisplayObjectContainer();
+    c.addChild(s);
+    for (var i = 0; i < 1024; i++) {
+      var p = new DisplayObjectContainer();
+      p.addChild(c);
+      c = p;
+    }
+    stage.addChild(p);
+    s.parent.removeChild(s);
+    eq(r, "AR");
+  });
+
+  unitTests.push(function () {
+    Random.seed(0x12343);
+    var stage = new Stage();
+    var s = new Shape();
+    var r = "";
+    s.addEventListener(Event.ADDED_TO_STAGE, function () {
+      r += "A";
+    });
+    s.addEventListener(Event.REMOVED_FROM_STAGE, function () {
+      r += "R";
+    });
+    var c = new DisplayObjectContainer();
+    c.addChild(s);
+    stage.addChild(c);
+    eq(r, "A");
+  });
 })();
