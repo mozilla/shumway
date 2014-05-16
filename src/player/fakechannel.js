c06a10a26780e1c25a01b8d699b4d9950793109c
Yury Delendik <ydelendik@mozilla.com>
2014-05-16 08:52:02 -0500

    Passthrough buffers in the fakechannel.js

--- src/player/fakechannel.js
+++ src/player/fakechannel.js
@@ -1,4 +1,21 @@
-// this is temporary worker to test postMessage tranfers
+// this is temporary worker to test postMessage transfers
+
+function collectTransfers(obj, transfers) {
+  if (typeof obj === 'object' && obj !== null) {
+    if ('buffer' in obj && obj.buffer instanceof ArrayBuffer) {
+      if (transfers.indexOf(obj.buffer) < 0) {
+        transfers.push(obj.buffer);
+      }
+    } else {
+      for (var i in obj) {
+        collectTransfers(obj[i], transfers);
+      }
+    }
+  }
+}
+
 addEventListener('message', function (e) {
-  postMessage(e.data);
+  var transfers = [];
+  collectTransfers(e.data, transfers);
+  postMessage(e.data, transfers);
 });
