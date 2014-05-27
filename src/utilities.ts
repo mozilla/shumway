a6fc62d0557e6b8b5f6164275ff309c7849c1976
Till Schneidereit <till@tillschneidereit.net>
2014-05-27 15:44:16 +0200

    Add declaration of Uint8ClampedArray to utilities.ts
    Missing from tsc's lib.d.ts.

--- src/utilities.ts
+++ src/utilities.ts
@@ -77,6 +77,26 @@ interface Error {
   stack: string;
 }
 
+interface Uint8ClampedArray extends ArrayBufferView {
+  BYTES_PER_ELEMENT: number;
+  length: number;
+  [index: number]: number;
+  get(index: number): number;
+  set(index: number, value: number): void;
+  set(array: Uint8Array, offset?: number): void;
+  set(array: number[], offset?: number): void;
+  subarray(begin: number, end?: number): Uint8ClampedArray;
+}
+
+declare var Uint8ClampedArray: {
+  prototype: Uint8ClampedArray;
+  new (length: number): Uint8ClampedArray;
+  new (array: Uint8Array): Uint8ClampedArray;
+  new (array: number[]): Uint8ClampedArray;
+  new (buffer: ArrayBuffer, byteOffset?: number, length?: number): Uint8ClampedArray;
+  BYTES_PER_ELEMENT: number;
+}
+
 module Shumway {
 
   export enum CharacterCodes {
