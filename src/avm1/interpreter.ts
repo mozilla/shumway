mozilla/shumway

commit eb10cd8f843a7a31bbef66d8f6f9611eed84f650

Replace all uses of try/finally, because JS engines really do not like that

src/avm1/interpreter.ts
@@ -469,6 +469,7 @@ module Shumway.AVM1 {
 
     var scopeContainer = context.initialScope.create(scope);
     var savedContext = AS2Context.instance;
+    var caughtError;
     try {
       AS2Context.instance = context;
       context.isActive = true;
@@ -485,14 +486,17 @@ module Shumway.AVM1 {
         console.error('Disabling AVM1 execution');
         context.executionProhibited = true;
       }
-      throw e; // TODO shall we just ignore it?
-    } finally {
-      context.isActive = false;
-      context.defaultTarget = null;
-      context.currentTarget = null;
-      actionTracer.unindent();
-      actionTracer.message('ActionScript Execution Stops');
-      AS2Context.instance = savedContext;
+      caughtError = e;
+    }
+    context.isActive = false;
+    context.defaultTarget = null;
+    context.currentTarget = null;
+    actionTracer.unindent();
+    actionTracer.message('ActionScript Execution Stops');
+    AS2Context.instance = savedContext;
+    if (caughtError) {
+      // Note: this doesn't use `finally` because that's a no-go for performance.
+      throw caughtError; // TODO shall we just ignore it?
     }
   }
 
@@ -693,8 +697,8 @@ module Shumway.AVM1 {
         var savedIsActive = currentContext.isActive;
         var savedDefaultTarget = currentContext.defaultTarget;
         var savedCurrentTarget = currentContext.currentTarget;
-        try
-        {
+        var caughtError;
+        try {
           // switching contexts if called outside main thread
           AS2Context.instance = currentContext;
           if (!savedIsActive) {
@@ -712,13 +716,18 @@ module Shumway.AVM1 {
           }
           return interpretActions(actionsData, newScopeContainer,
             constantPool, registers);
-        } finally {
-          currentContext.defaultTarget = savedDefaultTarget;
-          currentContext.currentTarget = savedCurrentTarget;
-          currentContext.isActive = savedIsActive;
-          currentContext.stackDepth--;
-          actionTracer.unindent();
-          AS2Context.instance = savedContext;
+        } catch (e) {
+          caughtError = e;
+        }
+        currentContext.defaultTarget = savedDefaultTarget;
+        currentContext.currentTarget = savedCurrentTarget;
+        currentContext.isActive = savedIsActive;
+        currentContext.stackDepth--;
+        actionTracer.unindent();
+        AS2Context.instance = savedContext;
+        if (caughtError) {
+          // Note: this doesn't use `finally` because that's a no-go for performance.
+          throw caughtError;
         }
       });
 
@@ -896,28 +905,29 @@ module Shumway.AVM1 {
       var registers = ectx.registers;
 
       var savedTryCatchState = currentContext.isTryCatchListening;
+      var caughtError;
       try {
         currentContext.isTryCatchListening = true;
         interpretActions(tryBlock, scopeContainer, constantPool, registers);
       } catch (e) {
         currentContext.isTryCatchListening = savedTryCatchState;
-        if (!catchBlockFlag) {
-          throw e;
-        }
-        if (!(e instanceof AS2Error)) {
-          throw e;
-        }
-        if (typeof catchTarget === 'string') { // TODO catchIsRegisterFlag?
-          scope.asSetPublicProperty(catchTarget, e.error);
+        if (!catchBlockFlag || !(e instanceof AS2Error)) {
+          caughtError = e;
         } else {
-          registers[catchTarget] = e.error;
+          if (typeof catchTarget === 'string') { // TODO catchIsRegisterFlag?
+            scope.asSetPublicProperty(catchTarget, e.error);
+          } else {
+            registers[catchTarget] = e.error;
+          }
+          interpretActions(catchBlock, scopeContainer, constantPool, registers);
         }
-        interpretActions(catchBlock, scopeContainer, constantPool, registers);
-      } finally {
-        currentContext.isTryCatchListening = savedTryCatchState;
-        if (finallyBlockFlag) {
-          interpretActions(finallyBlock, scopeContainer, constantPool, registers);
-        }
+      }
+      currentContext.isTryCatchListening = savedTryCatchState;
+      if (finallyBlockFlag) {
+        interpretActions(finallyBlock, scopeContainer, constantPool, registers);
+      }
+      if (caughtError) {
+        throw caughtError;
       }
     }
 

src/avm2/runtime.ts
@@ -1442,9 +1442,13 @@ module Shumway.AVM2.Runtime {
         return function () {
           var savedDxns = Shumway.AVM2.AS.ASXML.defaultNamespace;
           try {
-            return fn.apply(this, arguments);
-          } finally {
+            var result = fn.apply(this, arguments);
             Shumway.AVM2.AS.ASXML.defaultNamespace = savedDxns;
+            return result;
+          } catch (e) {
+            // Note: this doesn't use `finally` because that's a no-go for performance.
+            Shumway.AVM2.AS.ASXML.defaultNamespace = savedDxns;
+            throw e;
           }
         };
       })(fn);

src/swf/parser/parser.ts
@@ -234,11 +234,14 @@ module Shumway.SWF.Parser {
           inflateBlock(stream, output, this._state.compression);
         } while (stream.pos < buffer.length && !output.completed);
       } catch (e) {
-        if (e !== InflateNoDataError) throw e; // re-throw non data errors
-      } finally {
         this._state.bitBuffer = stream.bitBuffer;
         this._state.bitLength = stream.bitLength;
+        if (e !== InflateNoDataError) {
+          throw e; // Re-throw non data errors.
+        }
       }
+      this._state.bitBuffer = stream.bitBuffer;
+      this._state.bitLength = stream.bitLength;
       buffer.removeHead(stream.pos);
 
       // push data downstream

commit 2497eb388bf2db6c88e968e45a9b45de79727034

Fix AVM1 function interpretation to correctly reset the context after execution
I regressed this in eb10cd8f843a7a31bbef66d8f6f9611eed84f650 :/

src/avm1/interpreter.ts
@@ -668,6 +668,7 @@ module Shumway.AVM1 {
         var savedIsActive = currentContext.isActive;
         var savedDefaultTarget = currentContext.defaultTarget;
         var savedCurrentTarget = currentContext.currentTarget;
+        var result;
         var caughtError;
         try {
           // switching contexts if called outside main thread
@@ -681,12 +682,10 @@ module Shumway.AVM1 {
           currentContext.defaultTarget = defaultTarget;
           currentContext.currentTarget = null;
           actionTracer.indent();
-          currentContext.stackDepth++;
-          if (currentContext.stackDepth >= MAX_AVM1_STACK_LIMIT) {
+          if (++currentContext.stackDepth >= MAX_AVM1_STACK_LIMIT) {
             throw new AS2CriticalError('long running script -- AVM1 recursion limit is reached');
           }
-          return interpretActions(actionsData, newScopeContainer,
-            constantPool, registers);
+          result = interpretActions(actionsData, newScopeContainer, constantPool, registers);
         } catch (e) {
           caughtError = e;
         }
@@ -700,6 +699,7 @@ module Shumway.AVM1 {
           // Note: this doesn't use `finally` because that's a no-go for performance.
           throw caughtError;
         }
+        return result;
       });
 
       ownerClass = fn;