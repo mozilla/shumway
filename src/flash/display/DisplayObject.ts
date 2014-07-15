3d6172cc7758b48983ad701447749cb1ed92566e
Till Schneidereit <till@tillschneidereit.net>
2014-07-16 01:15:02 +0200

    Inline single-line method DisplayObject#_dirtyMatrix
    We manually set dirty flags all over the place, and this abstraction doesn't gain us anything.

--- src/flash/display/DisplayObject.ts
+++ src/flash/display/DisplayObject.ts
@@ -727,7 +727,7 @@ module Shumway.AVM2.AS.flash.display {
       this._rotation = DisplayObject._clampRotation(matrix.getRotation() * 180 / Math.PI);
       this._removeFlags(DisplayObjectFlags.InvalidMatrix);
       this._setFlags(DisplayObjectFlags.InvalidInvertedMatrix);
-      this._dirtyMatrix();
+      this._setDirtyFlags(DisplayObjectFlags.DirtyMatrix);
       this._invalidatePosition();
     }
 
@@ -872,17 +872,10 @@ module Shumway.AVM2.AS.flash.display {
     }
 
     /**
-     * Sets the |DirtyMatrix| flag.
-     */
-    private _dirtyMatrix() {
-      this._setDirtyFlags(DisplayObjectFlags.DirtyMatrix);
-    }
-
-    /**
      * Marks this object as having its matrix changed.
      */
     private _invalidateMatrix() {
-      this._dirtyMatrix();
+      this._setDirtyFlags(DisplayObjectFlags.DirtyMatrix);
       this._setFlags(DisplayObjectFlags.InvalidMatrix | DisplayObjectFlags.InvalidInvertedMatrix);
       if (this._parent) {
         this._parent._propagateFlags(DisplayObjectFlags.DirtyChild, Direction.Upward);
@@ -965,7 +958,7 @@ module Shumway.AVM2.AS.flash.display {
       this._matrix.tx = value;
       this._invertedMatrix.tx = -value;
       this._invalidatePosition();
-      this._dirtyMatrix();
+      this._setDirtyFlags(DisplayObjectFlags.DirtyMatrix);
     }
 
     get y(): number {
@@ -981,7 +974,7 @@ module Shumway.AVM2.AS.flash.display {
       this._matrix.ty = value;
       this._invertedMatrix.ty = -value;
       this._invalidatePosition();
-      this._dirtyMatrix();
+      this._setDirtyFlags(DisplayObjectFlags.DirtyMatrix);
     }
 
     get scaleX(): number {
