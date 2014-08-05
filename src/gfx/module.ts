62872e053d2a356e7f567ad9c719063e4e8fe618
Tobias Schneider <schneider@jancona.com>
2014-08-05 11:13:23 -0700

    Better buffer resizing and data writing in the Path2D polyfill

--- src/gfx/module.ts
+++ src/gfx/module.ts
@@ -42,6 +42,20 @@ module Shumway.GFX {
     profile && timelineBuffer && timelineBuffer.leave(name, data);
   }
 
+  enum PathCommand {
+    ClosePath = 1,
+    MoveTo,
+    LineTo,
+    QuadraticCurveTo,
+    BezierCurveTo,
+    ArcTo,
+    Rect,
+    Arc,
+    Save,
+    Restore,
+    Transform
+  }
+
   /**
    * Polyfill for missing |Path2D|. An instance of this class keeps a record of all drawing commands
    * ever called on it.
@@ -63,41 +77,41 @@ module Shumway.GFX {
       context.beginPath();
       while (i < commands.length) {
         switch (commands[i++]) {
-          case PathCommand.closePath:
+          case PathCommand.ClosePath:
             context.closePath();
             break;
-          case PathCommand.moveTo:
+          case PathCommand.MoveTo:
             context.moveTo(data[j++], data[j++]);
             break;
-          case PathCommand.lineTo:
+          case PathCommand.LineTo:
             context.lineTo(data[j++], data[j++]);
             break;
-          case PathCommand.quadraticCurveTo:
+          case PathCommand.QuadraticCurveTo:
             context.quadraticCurveTo(data[j++], data[j++], data[j++], data[j++]);
             break;
-          case PathCommand.bezierCurveTo:
+          case PathCommand.BezierCurveTo:
             context.bezierCurveTo(
               data[j++], data[j++], data[j++], data[j++], data[j++], data[j++]
             );
             break;
-          case PathCommand.arcTo:
+          case PathCommand.ArcTo:
             context.arcTo(data[j++], data[j++], data[j++], data[j++], data[j++]);
             break;
-          case PathCommand.rect:
+          case PathCommand.Rect:
             context.rect(data[j++], data[j++], data[j++], data[j++]);
             break;
-          case PathCommand.arc:
+          case PathCommand.Arc:
             context.arc(
               data[j++], data[j++], data[j++], data[j++], data[j++], !!data[j++]
             );
             break;
-          case PathCommand.save:
+          case PathCommand.Save:
             context.save();
             break;
-          case PathCommand.restore:
+          case PathCommand.Restore:
             context.restore();
             break;
-          case PathCommand.transform:
+          case PathCommand.Transform:
             context.transform(
               data[j++], data[j++], data[j++], data[j++], data[j++], data[j++]
             );
@@ -119,62 +133,74 @@ module Shumway.GFX {
     private _writeCommand(command: number) {
       var commands = this._commands;
       if (this._commandPosition >= commands.length) {
-        commands = new Uint8Array(commands.length * 2);
+        commands = new Uint8Array(((commands.length * 3) >> 1) + 1);
         commands.set(this._commands);
         this._commands = commands;
       }
       commands[this._commandPosition++] = command;
     }
 
-    private _writeData(a?: number, b?: number, c?: number, d?: number,
-                       e?: number, f?: number, g?: number, h?: number) {
+    private _writeData(a: number, b: number, c?: number, d?: number, e?: number, f?: number, g?: number) {
       var data = this._data;
-      if (this._dataPosition + arguments.length >= data.length) {
-        data = new Float32Array(data.length * 2);
+      var argc = arguments.length;
+      if (this._dataPosition + argc >= data.length) {
+        data = new Float32Array(((data.length * 3) >> 1) + 1);
         data.set(this._data);
         this._data = data;
       }
-      for (var i = 0; i < arguments.length; i++) {
-        data[this._dataPosition++] = arguments[i];
+      var p = this._dataPosition;
+      data[p] = a;
+      data[p + 1] = b;
+      if (argc > 2) {
+        data[p + 2] = c;
+        data[p + 3] = d;
       }
+      if (argc > 4) {
+        data[p + 4] = e;
+        data[p + 5] = f;
+      }
+      if (argc > 5) {
+        data[p + 6] = g;
+      }
+      this._dataPosition += argc;
     }
 
     closePath() {
-      this._writeCommand(PathCommand.closePath);
+      this._writeCommand(PathCommand.ClosePath);
     }
 
     moveTo(x: number, y: number) {
-      this._writeCommand(PathCommand.moveTo);
+      this._writeCommand(PathCommand.MoveTo);s
       this._writeData(x, y);
     }
 
     lineTo(x: number, y:number) {
-      this._writeCommand(PathCommand.lineTo);
+      this._writeCommand(PathCommand.LineTo);
       this._writeData(x, y);
     }
 
     quadraticCurveTo(cpx: number, cpy: number, x: number, y: number) {
-      this._writeCommand(PathCommand.quadraticCurveTo);
+      this._writeCommand(PathCommand.QuadraticCurveTo);
       this._writeData(cpx, cpy, x, y);
     }
 
     bezierCurveTo(cp1x: number, cp1y: number, cp2x: number, cp2y: number, x: number, y: number) {
-      this._writeCommand(PathCommand.bezierCurveTo);
+      this._writeCommand(PathCommand.BezierCurveTo);
       this._writeData(cp1x, cp1y, cp2x, cp2y, x, y);
     }
 
     arcTo(x1: number, y1: number, x2: number, y2: number, radiusX: number, radiusY: number, rotation: number) {
-      this._writeCommand(PathCommand.arcTo);
+      this._writeCommand(PathCommand.ArcTo);
       this._writeData(x1, y1, x2, y2, radiusX, radiusY, rotation);
     }
 
     rect(x: number, y: number, width: number, height: number) {
-      this._writeCommand(PathCommand.rect);
+      this._writeCommand(PathCommand.Rect);
       this._writeData(x, y, width, height);
     }
 
     arc(x: number, y: number, radius: number, startAngle: number, endAngle: number, anticlockwise: boolean) {
-      this._writeCommand(PathCommand.arc);
+      this._writeCommand(PathCommand.Arc);
       this._writeData(x, y, radius, startAngle, endAngle, +anticlockwise);
     }
 
@@ -185,8 +211,8 @@ module Shumway.GFX {
       var commands = this._commands;
       var data = this._data;
       if (transformation) {
-        this._writeCommand(PathCommand.save);
-        this._writeCommand(PathCommand.transform);
+        this._writeCommand(PathCommand.Save);
+        this._writeCommand(PathCommand.Transform);
         this._writeData(
           transformation.a,
           transformation.b,
@@ -201,23 +227,9 @@ module Shumway.GFX {
       }
       this._writeData.apply(this, path._data);
       if (transformation) {
-        this._writeCommand(PathCommand.restore);
+        this._writeCommand(PathCommand.Restore);
       }
     }
-  }
-
-  enum PathCommand {
-    closePath = 1,
-    moveTo = 2,
-    lineTo = 3,
-    quadraticCurveTo = 4,
-    bezierCurveTo = 5,
-    arcTo = 6,
-    rect = 7,
-    arc = 8,
-    save = 9,
-    restore = 10,
-    transform = 11
   }
 
   if (typeof CanvasRenderingContext2D !== 'undefined' && typeof Path2D === 'undefined') {
