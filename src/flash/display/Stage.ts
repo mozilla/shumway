mozilla/shumway

commit d6b57e8e0218eada7889a034bd3df0b9558d5374

Fix various Stage accessors' behavior
Specifically, `stageWidth`, `stageHeight`, `stageVideos`, and `displayState`.

Also adds a few Stage unit tests.

src/flash/display/Stage.ts
@@ -48,7 +48,7 @@ module Shumway.AVM2.AS.flash.display {
       this._displayState = null;
       this._fullScreenSourceRect = null;
       this._mouseLock = false;
-      this._stageVideos = null; // TODO
+      this._stageVideos = new GenericVector(0, true, ASObject);
       this._stage3Ds = null; // TODO
       this._colorARGB = 0xFFFFFFFF;
       this._fullScreenWidth = 0;
@@ -143,7 +143,17 @@ module Shumway.AVM2.AS.flash.display {
     }
 
     set stageWidth(value: number /*int*/) {
-      this._stageWidth = (value * 20) | 0;
+      // While the setter doesn't change the stored value, it still coerces the `value` parameter.
+      // This is script-visible if the value is something like `{valueOf: function(){throw 1}}`.
+      value = value|0;
+    }
+
+    /**
+     * Non-AS3-available setter. In AS3, the `stageWidth` setter is silently ignored.
+     */
+    setStageWidth(value: number) {
+      release || assert ((value|0) === value);
+      this._stageWidth = value * 20|0;
     }
 
     get stageHeight(): number /*int*/ {
@@ -151,7 +161,17 @@ module Shumway.AVM2.AS.flash.display {
     }
 
     set stageHeight(value: number /*int*/) {
-      this._stageHeight = (value * 20) | 0;
+      // While the setter doesn't change the stored value, it still coerces the `value` parameter.
+      // This is script-visible if the value is something like `{valueOf: function(){throw 1}}`.
+      value = value|0;
+    }
+
+    /**
+     * Non-AS3-available setter. In AS3, the `stageHeight` setter is silently ignored.
+     */
+    setStageHeight(value: number) {
+      release || assert ((value|0) === value);
+      this._stageHeight = value * 20|0;
     }
 
     get showDefaultContextMenu(): boolean {
@@ -210,8 +230,8 @@ module Shumway.AVM2.AS.flash.display {
     }
 
     set displayState(value: string) {
-      //this._displayState = asCoerceString(value);
-      notImplemented("public flash.display.Stage::set displayState"); return;
+      somewhatImplemented("public flash.display.Stage::set displayState");
+      this._displayState = asCoerceString(value);
     }
 
     get fullScreenSourceRect(): flash.geom.Rectangle {

src/player/player.ts
@@ -135,8 +135,8 @@ module Shumway.Player {
           var root = loader.content;
           stage._loaderInfo = loaderInfo;
           stage.frameRate = loaderInfo.frameRate;
-          stage.stageWidth = loaderInfo.width;
-          stage.stageHeight = loaderInfo.height;
+          stage.setStageWidth(loaderInfo.width);
+          stage.setStageHeight(loaderInfo.height);
           stage.color = ColorUtilities.RGBAToARGB(loaderInfo._colorRGBA);
           stage.addChildAtDepth(root, 0);
           self._enterLoops();
@@ -370,8 +370,8 @@ module Shumway.Player {
       });
 
       loaderInfo.addEventListener(flash.events.Event.COMPLETE, function onProgress() {
-        stage.stageWidth = 1024;
-        stage.stageHeight = 1024;
+        stage.setStageWidth(1024);
+        stage.setStageHeight(1024);
 
         var symbols = [];
         loaderInfo._dictionary.forEach(function (symbol, key) {

test/unit/fail/loader.js
@@ -61,8 +61,8 @@
       check(l.content, "We should have some content here.");
       check(l.content.root === l.content, "This should be the root.");
       check(DisplayObject.isType(l.content));
-      s.stageWidth = l.contentLoaderInfo.width;
-      s.stageHeight = l.contentLoaderInfo.height;
+      s.setStageWidth(l.contentLoaderInfo.width);
+      s.setStageHeight(l.contentLoaderInfo.height);
       s.frameRate = l.contentLoaderInfo.frameRate;
       s.addChild(l.content);
       check(s.frameRate);

test/unit/pass/mouse.js
@@ -12,8 +12,8 @@
   function createMouseDispatcher() {
     var s = new Stage();
     var m = new MouseEventDispatcher();
-    s.stageWidth = 550;
-    s.stageHeight = 400;
+    s.setStageWidth(550);
+    s.setStageHeight(400);
     m.stage = s;
     return m;
   }

test/unit/pass/stage.js
@@ -2,6 +2,7 @@
   var Stage = flash.display.Stage;
   var DisplayObject = flash.display.DisplayObject;
   var DisplayObjectContainer = flash.display.DisplayObjectContainer;
+  var StageVideo = flash.media.StageVideo;
 
   function createDisplayObjectTree(depth, width, height) {
     var nodes = [];
@@ -31,6 +32,8 @@
 
   unitTests.push(function checkDefaults() {
     var stage = new Stage();
+    stage.setStageWidth(550);
+    stage.setStageHeight(450);
     eq(stage.root, stage, "root");
     check(stage.scaleMode === "showAll", "scaleMode");
     eq(stage.stage, stage, "stage");
@@ -58,10 +61,11 @@
     check(stage.blendMode === "normal", "blendMode");
     check(stage.transform, "transform");
     eq(stage.frameRate, 24, "frameRate");
+    stage.frameRate = 10;
+    eq(stage.frameRate, 10, "frameRate");
     //check(o.numChildren === 1, "numChildren");
     eq(stage.scale9Grid, null, "scale9Grid");
     //check(o.constructor === null, "constructor");
-    //check(o.stageWidth === 550, "stageWidth");
     eq(stage.color, 4294967295, "color");
     eq(stage.tabChildren, true, "tabChildren");
     //check(o.quality === "HIGH", "quality");
@@ -69,11 +73,17 @@
     //check(o.loaderInfo, "loaderInfo");
     eq(stage.focus, null, "focus");
     check(stage.colorCorrection === "default", "colorCorrection");
+    eq(stage.width, 0, "width");
+    eq(stage.stageWidth, 550, "stageWidth");
+    stage.stageWidth = 300;
+    eq(stage.stageWidth, 550, "stageWidth setter is ignored");
     eq(stage.height, 0, "height");
+    eq(stage.stageHeight, 450, "stageHeight");
+    stage.stageHeight = 300;
+    eq(stage.stageHeight, 450, "stageHeight setter is ignored");
     //check(o.name === null, "name");
     check(stage.colorCorrectionSupport === "defaultOff", "colorCorrectionSupport");
     eq(stage.showDefaultContextMenu, true, "showDefaultContextMenu");
-    eq(stage.width, 0, "width");
     //check(o.accessibilityProperties === null, "accessibilityProperties");
     check(stage.align === "", "align");
     eq(stage.displayState, null, "displayState");
@@ -81,9 +91,10 @@
     eq(stage.mouseLock, false, "mouseLock");
     eq(stage.stageFocusRect, true, "stageFocusRect");
     eq(stage.fullScreenWidth, 0, "fullScreenWidth");
-    eq(stage.z, 0, "z");
     eq(stage.fullScreenHeight, 0, "fullScreenHeight");
     eq(stage.x, 0, "x");
+    eq(stage.y, 0, "y");
+    eq(stage.z, 0, "z");
     check(stage.softKeyboardRect, "softKeyboardRect");
     eq(stage.wmodeGPU, false, "wmodeGPU");
     eq(stage.allowsFullScreen, false, "allowsFullScreen");
@@ -91,9 +102,10 @@
     eq(stage.contentsScaleFactor, 1, "contentsScaleFactor");
     eq(stage.alpha, 1, "alpha");
     //check(o.metaData === null, "metaData");
-    eq(stage.y, 0, "y");
     eq(stage.displayContextInfo, null, "displayContextInfo");
     eq(stage.parent, null, "parent");
+    // TODO: check that stage.stageVideos is of type Vector.<StageVideo>.
+    eq(stage.stageVideos.length, 0, "stageVideos");
 
     stage.color = 0x12345678;
     eq(stage.color | 0,  0xff345678 | 0, "opaque");

commit 9872f41a0a08c199889b64df92a7b4be60df97aa

Fixes lint regressions from d6b57e8e0218ead

src/flash/display/Stage.ts
@@ -145,15 +145,15 @@ module Shumway.AVM2.AS.flash.display {
     set stageWidth(value: number /*int*/) {
       // While the setter doesn't change the stored value, it still coerces the `value` parameter.
       // This is script-visible if the value is something like `{valueOf: function(){throw 1}}`.
-      value = value|0;
+      value = value | 0;
     }
 
     /**
      * Non-AS3-available setter. In AS3, the `stageWidth` setter is silently ignored.
      */
     setStageWidth(value: number) {
-      release || assert ((value|0) === value);
-      this._stageWidth = value * 20|0;
+      release || assert ((value | 0) === value);
+      this._stageWidth = (value * 20) | 0;
     }
 
     get stageHeight(): number /*int*/ {
@@ -163,15 +163,15 @@ module Shumway.AVM2.AS.flash.display {
     set stageHeight(value: number /*int*/) {
       // While the setter doesn't change the stored value, it still coerces the `value` parameter.
       // This is script-visible if the value is something like `{valueOf: function(){throw 1}}`.
-      value = value|0;
+      value = value | 0;
     }
 
     /**
      * Non-AS3-available setter. In AS3, the `stageHeight` setter is silently ignored.
      */
     setStageHeight(value: number) {
-      release || assert ((value|0) === value);
-      this._stageHeight = value * 20|0;
+      release || assert ((value | 0) === value);
+      this._stageHeight = (value * 20) | 0;
     }
 
     get showDefaultContextMenu(): boolean {