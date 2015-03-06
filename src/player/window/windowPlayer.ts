mozilla/shumway

commit 1baa32279afec96d7a464d6ca67dfaf4419ce9ff

Refactors registerFontOrImage -- don't pass Promise from sandbox.

src/player/window/windowPlayer.ts
@@ -23,6 +23,7 @@ module Shumway.Player.Window {
   export class WindowGFXService extends GFXServiceBase {
     private _window;
     private _parent;
+    private _fontOrImageRequests: PromiseWrapper<any>[];
 
     constructor(window, parent?) {
       super();
@@ -35,6 +36,7 @@ module Shumway.Player.Window {
       this._window.addEventListener('syncmessage', function (e) {
         this.onWindowMessage(e.detail, false);
       }.bind(this));
+      this._fontOrImageRequests = [];
     }
 
     update(updates: DataBuffer, assets: any[]): void {
@@ -85,19 +87,35 @@ module Shumway.Player.Window {
       return event.detail.result;
     }
 
-    registerFontOrImage(syncId: number, symbolId: number, type: string, data: any): Promise<any> {
-      return new Promise<any>(function (resolve, reject) {
-        var event = this._parent.document.createEvent('CustomEvent');
-        event.initCustomEvent('syncmessage', false, false, {
-          type: 'registerFontOrImage',
-          syncId: syncId,
-          symbolId: symbolId,
-          assetType: type,
-          data: data,
-          resolve: resolve
-        });
-        this._parent.dispatchEvent(event);
-      }.bind(this));
+    registerFont(syncId: number, data: any): Promise<any> {
+      var requestId = this._fontOrImageRequests.length;
+      var result = new PromiseWrapper<any>();
+      this._fontOrImageRequests[requestId] = result;
+      var message = {
+        type: 'registerFontOrImage',
+        syncId: syncId,
+        assetType: 'font',
+        data: data,
+        requestId: requestId
+      };
+      this._parent.postMessage(message, '*');
+      return result.promise;
+    }
+
+    registerImage(syncId: number, symbolId: number, data: any): Promise<any> {
+      var requestId = this._fontOrImageRequests.length;
+      var result = new PromiseWrapper<any>();
+      this._fontOrImageRequests[requestId] = result;
+      var message = {
+        type: 'registerFontOrImage',
+        syncId: syncId,
+        symbolId: symbolId,
+        assetType: 'image',
+        data: data,
+        requestId: requestId
+      };
+      this._parent.postMessage(message, '*');
+      return result.promise;
     }
 
     fscommand(command: string, args: string): void {

src/base/remoting.ts
@@ -214,7 +214,8 @@ module Shumway.Remoting {
     updateAndGet(updates: DataBuffer, assets: Array<DataBuffer>): any;
     frame(): void;
     videoControl(id: number, eventType: VideoControlEvent, data: any): any;
-    registerFontOrImage(syncId: number, symbolId: number, type: string, data: any): Promise<any>;
+    registerFont(syncId: number, data: any): Promise<any>;
+    registerImage(syncId: number, symbolId: number, data: any): Promise<any>;
     fscommand(command: string, args: string): void;
   }
 }

src/flash/display/Loader.ts
@@ -472,9 +472,8 @@ module Shumway.AVM2.AS.flash.display {
       var symbol = BitmapSymbol.FromData(data);
       this._imageSymbol = symbol;
       var resolver: Timeline.IAssetResolver = AVM2.instance.globals['Shumway.Player.Utils'];
-      resolver.registerFontOrImage(symbol, data);
+      resolver.registerImage(symbol, data);
       release || assert(symbol.resolveAssetPromise);
-      release || assert(symbol.ready === false);
     }
 
     private _applyDecodedImage(symbol: BitmapSymbol) {

src/flash/display/LoaderInfo.ts
@@ -370,10 +370,23 @@ module Shumway.AVM2.AS.flash.display {
       release || assert(symbol, "Unknown symbol type " + data.type);
       this._dictionary[id] = symbol;
       if (symbol.ready === false) {
-        var resolver: Timeline.IAssetResolver = AVM2.Runtime.AVM2.instance.globals['Shumway.Player.Utils'];
-        resolver.registerFontOrImage(<Timeline.EagerlyResolvedSymbol><any>symbol, data);
+        this._registerFontOrImage(<Timeline.EagerlyResolvedSymbol><any>symbol, data);
       }
       return symbol;
+    }
+
+    private _registerFontOrImage(symbol: Timeline.EagerlyResolvedSymbol, data: any) {
+      var resolver: Timeline.IAssetResolver = AVM2.Runtime.AVM2.instance.globals['Shumway.Player.Utils'];
+      switch (data.type) {
+        case 'font':
+          resolver.registerFont(<Timeline.EagerlyResolvedSymbol><any>symbol, data);
+          break;
+        case 'image':
+          resolver.registerImage(<Timeline.EagerlyResolvedSymbol><any>symbol, data);
+          break;
+        default:
+          throw new Error('Unsupported assert type: ' + data.type);
+      }
     }
 
     getRootSymbol(): flash.display.SpriteSymbol {

src/flash/symbol.ts
@@ -27,7 +27,8 @@ module Shumway.Timeline {
   import ActionScriptVersion = flash.display.ActionScriptVersion;
 
   export interface IAssetResolver {
-    registerFontOrImage(symbol: Timeline.EagerlyResolvedSymbol, data: any): void;
+    registerFont(symbol: Timeline.EagerlyResolvedSymbol, data: any): void;
+    registerImage(symbol: Timeline.EagerlyResolvedSymbol, data: any): void;
   }
 
   export interface EagerlyResolvedSymbol {

src/gfx/test/testEaselHost.ts
@@ -91,6 +91,14 @@ module Shumway.GFX.Test {
       return Promise.resolve(buffer);
     }
 
+    private _sendRegisterFontOrImageResponse(requestId: number, result: any) {
+      this._worker.postMessage({
+        type: 'registerFontOrImageResponse',
+        requestId: requestId,
+        result: result
+      });
+    }
+
     _onWorkerMessage(e, async: boolean = true): any {
       var data = e.data;
       if (typeof data !== 'object' || data === null) {
@@ -118,7 +126,7 @@ module Shumway.GFX.Test {
           break;
         case 'registerFontOrImage':
           this.processRegisterFontOrImage(data.syncId, data.symbolId, data.assetType, data.data,
-                                          data.resolve);
+            this._sendRegisterFontOrImageResponse.bind(this, data.requestId));
           e.handled = true;
           break;
         case 'fscommand':

src/gfx/window/windowEaselHost.ts
@@ -80,6 +80,14 @@ module Shumway.GFX.Window {
       }.bind(this));
     }
 
+    private _sendRegisterFontOrImageResponse(requestId: number, result: any) {
+      this._playerWindow.postMessage({
+        type: 'registerFontOrImageResponse',
+        requestId: requestId,
+        result: result
+      }, '*');
+    }
+
     private onWindowMessage(data, async: boolean = true) {
       if (typeof data === 'object' && data !== null) {
         if (data.type === 'player') {
@@ -97,7 +105,7 @@ module Shumway.GFX.Window {
           data.result = this.processVideoControl(data.id, data.eventType, data.data);
         } else if (data.type === 'registerFontOrImage') {
           this.processRegisterFontOrImage(data.syncId, data.symbolId, data.assetType, data.data,
-                                          data.resolve);
+            this._sendRegisterFontOrImageResponse.bind(this, data.requestId));
         } else if (data.type === 'fscommand') {
           this.processFSCommand(data.command, data.args);
         } else if (data.type === 'timelineResponse' && data.timeline) {

src/player/player.ts
@@ -87,7 +87,11 @@ module Shumway.Player {
       throw new Error('This method is abstract');
     }
 
-    registerFontOrImage(syncId: number, symbolId: number, type: string, data: any): Promise<any> {
+    registerFont(syncId: number, data: any): Promise<any> {
+      throw new Error('This method is abstract');
+    }
+
+    registerImage(syncId: number, symbolId: number, data: any): Promise<any> {
       throw new Error('This method is abstract');
     }
 
@@ -688,18 +692,27 @@ module Shumway.Player {
       });
     }
 
-    registerFontOrImage(symbol: Timeline.EagerlyResolvedSymbol, data: any): void {
+    registerFont(symbol: Timeline.EagerlyResolvedSymbol, data: any): void {
       release || assert(symbol.syncId);
       symbol.resolveAssetPromise = new PromiseWrapper(); // TODO no need for wrapper here, change to Promise
-      this._gfxService.registerFontOrImage(symbol.syncId, symbol.id, data.type, data).then(function (result) {
+      this._gfxService.registerFont(symbol.syncId, data).then(function (result) {
         symbol.resolveAssetPromise.resolve(result);
       });
       // Fonts are immediately available in Firefox, so we can just mark the symbol as ready.
-      if (data.type === 'font' && inFirefox) {
+      if (inFirefox) {
         symbol.ready = true;
       } else {
         symbol.resolveAssetPromise.then(symbol.resolveAssetCallback, null);
       }
+    }
+
+    registerImage(symbol: Timeline.EagerlyResolvedSymbol, data: any): void {
+      release || assert(symbol.syncId);
+      symbol.resolveAssetPromise = new PromiseWrapper(); // TODO no need for wrapper here, change to Promise
+      this._gfxService.registerImage(symbol.syncId, symbol.id, data).then(function (result) {
+        symbol.resolveAssetPromise.resolve(result);
+      });
+      symbol.resolveAssetPromise.then(symbol.resolveAssetCallback, null);
     }
   }
 }

commit 12f5c51e82aa87cbd89de98c760525b826b467c6

Fixes iframe regression from 1baa32279afec

src/player/window/windowPlayer.ts
@@ -140,6 +140,12 @@ module Shumway.Player.Window {
           case 'displayParameters':
             this.processDisplayParameters(data.params);
             break;
+          case 'registerFontOrImageResponse':
+            var request = this._fontOrImageRequests[data.requestId];
+            release || Debug.assert(request);
+            delete this._fontOrImageRequests[data.requestId];
+            request.resolve(data.result);
+            break;
           case 'options':
             Shumway.Settings.setSettings(data.settings);
             break;