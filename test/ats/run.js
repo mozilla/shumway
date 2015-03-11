3b40ba767bb08bca24d054026769e18f01aee8a6
Tobias Schneider <schneider@jancona.com>
2015-03-10 17:16:03 -0700

    Initial automated testing setup

---
+++ test/ats/run.js
@@ -0,0 +1,119 @@
+var CACHE_DIR = "./swfs/";
+var DOWNLOAD_BASE = "http://areweflashyet.com/swfs/";
+
+var taskName = "list";
+var conditions = { };
+
+// TODO: Accept proper command line options.
+if (process.argv.length > 2) {
+  taskName = process.argv[2];
+  if (process.argv.length > 3) {
+    conditions.file = { $regex: process.argv[3] };
+    token = process.argv[4] || null;
+  }
+}
+
+var fs = require('fs');
+var http = require('http');
+var mongojs = require("mongojs");
+var task = require("./tasks/" + taskName);
+
+var db = mongojs(process.env.ATS_DB_URL || "ats", ["swfs"]);
+
+db.on('error', function(err) {
+  handle_error(err);
+});
+
+function get_file(fileName, cb) {
+  var path = CACHE_DIR + fileName;
+  fs.exists(path, function(exist) {
+    if (exist) {
+      cb(null, path);
+      return;
+    }
+    console.log('Download ' + fileName);
+    var file = fs.createWriteStream(path);
+    http.get(DOWNLOAD_BASE + fileName, function(response) {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close();
+        cb(null, path);
+      });
+    }).on('error', function (err) {
+      cb(err, null);
+    });
+  });
+}
+
+function handle_error(err) {
+  console.error(err.toString());
+  db.close();
+  process.exit(1);
+}
+
+var startTime = +new Date;
+function wrap_up() {
+  db.close();
+  console.log("Finished " + n + " tasks in " + ((+new Date - startTime) / 1000).toFixed(2) + "s.");
+}
+
+var swfs = db.swfs.find(conditions, { timeout: false });
+var i = 0;
+var n = 0;
+swfs.count(function (err, val) {
+  if (err) {
+    handle_error(err);
+  }
+  n = val;
+  swfs.next(next);
+});
+
+function next(err, swf) {
+  i++;
+  if (err) {
+    handle_error(err);
+  }
+  get_file(swf.file, function (err, path) {
+    if (err) {
+      handle_error(err);
+    } else {
+      swf.path = path;
+      var time = +new Date;
+      console.log("Run task " + i + " of " + n);
+      task.run(swf, function (err, result) {
+        var updates = Object.create(null);
+        updates[taskName + '_last'] = time;
+        if (err) {
+          if (taskName + '_result' in swf) {
+            updates[taskName + '_regressed'] = true;
+            updates.$unset = Object.create(null);
+            updates.$unset[taskName + '_result'] = true;
+          }
+          updates[taskName + '_error'] = err.message;
+        } else {
+          updates[taskName + '_result'] = result;
+          updates[taskName + '_time'] = +new Date - time;
+          if (taskName + '_error' in swf) {
+            updates.$unset = Object.create(null);
+            updates.$unset[taskName + '_error'] = true;
+            updates.$unset[taskName + '_regressed'] = true;
+          }
+        }
+        db.swfs.update(
+          { _id: swf._id },
+          { $set: updates },
+          function (err) {
+            if (err) {
+              handle_error(err);
+            }
+            if (i < n) {
+              swfs.next(next);
+            } else {
+              wrap_up();
+            }
+          }
+        );
+      });
+    }
+  });
+}
---
+++ test/ats/tasks/parse.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var Shumway = require('../../../build/bundles/shumway.player.js').Shumway;
+var FileLoader = Shumway.FileLoader;
+var CompressionMethod = Shumway.SWF.CompressionMethod;
+
+exports.run = function (info, cb) {
+  var result = Object.create(null);
+  var buffer = fs.readFileSync(info.path);
+  var loader = new Shumway.FileLoader({
+    onLoadOpen: function (file) {
+      if (file instanceof Shumway.SWF.SWFFile) {
+        var dictionary = file.dictionary;
+        for (var i = 0; i < dictionary.length; i++) {
+          if (dictionary[i]) {
+            var symbol = file.getSymbol(dictionary[i].id);
+          }
+        }
+        result.type = 'SWF';
+        result.file_size = file.bytesTotal;
+        result.compression = CompressionMethod[file.compression];
+        result.swf_version = file.swfVersion;
+        result.uses_avm1 = file.useAVM1;
+        result.frame_rate = file.frameRate;
+        result.frame_count = file.frameCount;
+        var bounds = file.bounds;
+        result.width = bounds.width / 20 | 0;
+        result.height = bounds.height / 20 | 0;
+      } else if (file instanceof Shumway.ImageFile) {
+        result.type = 'Image';
+        result.file_size = file.bytesTotal;
+        result.mime_type = file.mimeType;
+      } else {
+        throw new Error('Unknown file format');
+      }
+    },
+    onLoadProgress: function () { },
+    onNewEagerlyParsedSymbols: function (list, delta) {
+      return Promise.resolve();
+    },
+    onImageBytesLoaded: function () { }
+  });
+  try {
+    loader.loadBytes(new Uint8Array(buffer));
+    if (cb) {
+      cb(null, result);
+    }
+  } catch (e) {
+    console.error(e.toString());
+    if (cb) {
+      cb(e, null);
+    }
+  }
+};
