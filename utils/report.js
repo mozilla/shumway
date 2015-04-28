ad5266cfd01dda4758b672024864ee5901ca53db
Michael Bebenita <mbebenita@gmail.com>
2015-04-28 09:33:17 -0700

    Add reporting tool.

---
+++ utils/report.js
@@ -0,0 +1,80 @@
+var commandLineArguments;
+// SpiderMonkey
+if (typeof scriptArgs === "undefined") {
+  commandLineArguments = arguments;
+} else {
+  commandLineArguments = scriptArgs;
+}
+
+var fileCount = 0;
+var passCount = 0;
+var finishedCount = 0;
+var zeroHashCount = 0;
+
+var histogramNames = ["Not Implemented", "somewhatImplemented", "Uncaught VM-internal", "AVM1 error", "AVM1 warning", "Tag not handled by the parser", "Unable to resolve"]
+var histograms = {};
+
+commandLineArguments.forEach(function (file) {
+
+  var file = commandLineArguments[0];
+
+  read(file).toString().split("\n").forEach(function (k) {
+    if (k.indexOf("RUNNING:") === 0) {
+      fileCount++;
+    } else if (k.indexOf("HASHCODE:") === 0) {
+      if (k.indexOf(': 0x00000000') > 0) {
+        zeroHashCount++
+      } else {
+        passCount++
+      }
+      finishedCount++;
+    }
+    for (var i = 0; i < histogramNames.length; i++) {
+      var n = histogramNames[i];
+      if (k.indexOf(n) >= 0) {
+        if (!histograms[n]) histograms[n] = [];
+        histograms[n].push(k);
+      }
+    }
+    // print(k);
+  });
+});
+
+function histogram(array, min) {
+  min = min || 0;
+  var o = Object.create(null);
+  array.forEach(function (k) {
+    o[k] || (o[k] = 0);
+    o[k]++;
+  });
+  var a = [];
+  for (var k in o) {
+    a.push([k, o[k]]);
+  }
+  a = a.filter(function (x) {
+    return x[1] > min;
+  });
+  return a.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+}
+
+var failCount = fileCount - passCount;
+
+print("File Count: " + fileCount);
+print("Pass Count: " + passCount);
+print("Fail Count: " + failCount);
+print("Zero Hash Count: " + zeroHashCount);
+print("Finished Count: " + finishedCount);
+
+
+histogramNames.forEach(function (n) {
+  var h = histograms[n];
+  if (!h) return;
+  print();
+  print(n + " Count: " + h.length);
+  print();
+  histogram(h, 10).forEach(function (v) {
+    print(v[1] + ": " + v[0]);
+  });
+});
