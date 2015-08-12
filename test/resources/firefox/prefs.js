9e0928b6dae5742ab45877b5979ac480354044da
Yury Delendik <ydelendik@mozilla.com>
2015-08-12 16:19:50 -0500

    Bug 1193595 - Disables XPI verification during testing.

--- test/resources/firefox/prefs.js
+++ test/resources/firefox/prefs.js
@@ -3,6 +3,7 @@ user_pref("extensions.ui.lastCategory", "addons://list/extension");
 user_pref("browser.rights.3.shown", true);
 user_pref("shumway.ignoreCTP", true);
 user_pref("shumway.environment", "test");
+user_pref("xpinstall.signatures.required", false);
 user_pref("browser.displayedE10SPrompt", 5);
 user_pref("browser.displayedE10SPrompt.1", 5);
 user_pref("browser.displayedE10SNotice", 2);
